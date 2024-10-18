d78407c0a3e30cfb0f3d43a093af5497a32bcfe0
renpeng <renpeng@hellogroup.com>
2024-10-18 14:05:13 +0800

    add

--- src/20241018/128. 最长连续序列.go
+++ src/20241018/128. 最长连续序列.go
@@ -3,50 +3,33 @@ package main
 import "fmt"
 
 func longestConsecutive(nums []int) int {
-	set := make(map[int]struct{}, len(nums))
+	set := make(map[int]bool, len(nums))
 	for _, v := range nums {
-		set[v] = struct{}{}
+		set[v] = true
 	}
 
-	fmt.Println("set: ", set)
-
-	i := 0
 	max := 0
-	for i < len(nums) {
-		fmt.Printf("i :%+v nums[i]: %+v\n", i, nums[i])
-		if _, ok := set[nums[i]-1]; ok {
-			i++
-			fmt.Printf("existed in set nums[i]: %+v, nums[i]-1: %+v\n", nums[i], nums[i]-1)
-			continue
-		}
-		j := i + 1
-		step := 1
-		for j < len(nums) {
-			if _, ok := set[nums[i]+step]; ok {
-				step++
-				if j == len(nums)-1 {
-					if j-i-1 > max {
-						max = j - i - 1
-					}
-				}
-			} else {
-				fmt.Printf("max: %+v, (j-i-1): %+v \n", max, j-i-1)
-				if j-i-1 > max {
-					max = j - i - 1
+	for v := range set {
+		if !set[v-1] {
+			cur := v
+			cnt := 1
 
-				}
-				step = 0
+			for set[cur+1] {
+				cur++
+				cnt++
 			}
-			j++
+
+			if cnt > max {
+				max = cnt
+			}
 		}
-		i = j
 	}
 	return max
 }
 
 func main() {
-	data := []int{0, 3, 7, 2, 5, 8, 4, 6, 1}
-	// data := []int{100, 4, 200, 1, 3, 2}
+	// data := []int{0, 3, 7, 2, 5, 8, 4, 6, 1}
+	data := []int{100, 4, 200, 1, 3, 2}
 	res := longestConsecutive(data)
 	fmt.Println(res)
 }
