4a86acf2a5f62eab585167c34c08b413afca5fe9
renpeng <renpeng@hellogroup.com>
2024-10-22 14:37:25 +0800

    add

--- src/2024_1021~1027/3. 无重复字符的最长子串.go
+++ src/2024_1021~1027/3. 无重复字符的最长子串.go
@@ -14,10 +14,15 @@ func lengthOfLongestSubstring(s string) int {
 	set[string(s[0])] = 0
 	for ; tail < len(s) && head < len(s); tail++ {
 		if tail == len(s)-1 {
-			if tail-head > max {
-				max = tail - head
+			if _, ok := set[string(s[tail])]; !ok {
+				if tail-head+1 > max {
+					max = tail - head + 1
+				}
+			} else {
+				if tail-head > max {
+					max = tail - head
+				}
 			}
-			break
 		}
 		if _, ok := set[string(s[tail])]; !ok {
 			set[string(s[tail])] = tail
@@ -25,9 +30,12 @@ func lengthOfLongestSubstring(s string) int {
 			if tail-head > max {
 				max = tail - head
 			}
-			delete(set, string(s[head]))
-			set[string(s[tail])] = tail
 			head = set[string(s[tail])] + 1
+
+			set = make(map[string]int, len(s))
+			for i := head; i <= tail; i++ {
+				set[string(s[i])] = i
+			}
 		}
 	}
 	return max
