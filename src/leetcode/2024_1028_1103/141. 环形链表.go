1cf08cdc1766043140cadef674280eb7728b294e
renpeng <renpeng@p1.com>
2024-10-27 16:58:35 +0800

    add

--- src/leetcode/2024_1028_1103/141. 环形链表.go
+++ src/leetcode/2024_1028_1103/141. 环形链表.go
@@ -1,27 +1,34 @@
 package main
 
+func hasCycle1(head *ListNode) bool {
+	if head == nil {
+		return false
+	}
+	fast := head
+	slow := head
+	for fast.Next != nil && fast.Next.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			return true
+		}
+	}
+	return false
+}
+
 func hasCycle(head *ListNode) bool {
 	if head == nil {
 		return false
 	}
 
-	fast := head
-	slow := head
+	path := make(map[*ListNode]struct{})
 
-	for fast.Next != nil && fast.Next.Next != nil {
-		slow = slow.Next
-		fast = fast.Next.Next
-
-		if slow == fast {
+	for head != nil {
+		if _, ok := path[head]; ok {
 			return true
 		}
-
+		path[head] = struct{}{}
+		head = head.Next
 	}
-
 	return false
-
-}
-
-func main() {
-
 }
