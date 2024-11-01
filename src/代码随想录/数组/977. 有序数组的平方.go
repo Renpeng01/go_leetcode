3f7ee5404c0f651070223989d41e45cbf6466493
renpeng <renpeng@hellogroup.com>
2024-11-01 16:55:24 +0800

    add

--- src/代码随想录/数组/977. 有序数组的平方.go
+++ src/代码随想录/数组/977. 有序数组的平方.go
@@ -2,12 +2,58 @@ package main
 
 import "sort"
 
-func sortedSquares(nums []int) []int {
+func sortedSquares1(nums []int) []int {
 	newNums := make([]int, 0, len(nums))
 	for _, v := range nums {
 		newNums = append(newNums, v*v)
 	}
 
 	sort.Ints(newNums)
+	return newNums
+}
+
+// 双指针解法
+func sortedSquares(nums []int) []int {
+	negIndex := -1
+	for i, v := range nums {
+		if v < 0 {
+			negIndex = i
+		}
+	}
+
+	for i := 0; i < len(nums); i++ {
+		nums[i] = nums[i] * nums[i]
+	}
+	if negIndex == -1 {
+		return nums
+	}
+
+	newNums := make([]int, 0, len(nums))
+
+	aStart := negIndex
+	aEnd := 0
+
+	bStart := negIndex + 1
+	bEnd := len(nums) - 1
+
+	for aStart >= aEnd && bStart <= bEnd {
+		if nums[aStart] < nums[bStart] {
+			newNums = append(newNums, nums[aStart])
+			aStart--
+		} else {
+			newNums = append(newNums, nums[bStart])
+			bStart++
+		}
+	}
+
+	for bStart <= bEnd {
+		newNums = append(newNums, nums[bStart])
+		bStart++
+	}
+
+	for aStart >= aEnd {
+		newNums = append(newNums, nums[aStart])
+		aStart--
+	}
 	return newNums
 }
