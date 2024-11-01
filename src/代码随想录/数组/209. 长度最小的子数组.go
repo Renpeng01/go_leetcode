9874acceb1dcd01fb7a1d3c03f0ccdb7ba0925ca
renpeng <renpeng@hellogroup.com>
2024-11-01 17:51:53 +0800

    add

---
+++ src/代码随想录/数组/209. 长度最小的子数组.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func minSubArrayLen(target int, nums []int) int {
+	sumNums := make([]int, 0, len(nums))
+	sum := 0
+	for _, v := range nums {
+		if v == target {
+			return 1
+		}
+		sum += v
+		sumNums = append(sumNums, sum)
+	}
+
+	fmt.Println("sums: ", sumNums)
+
+	cnt := len(sumNums)
+	for i, v := range sumNums {
+		for j := i - 1; j >= 0; j-- {
+			if v-sumNums[j] == target {
+				if i-j < cnt {
+					cnt = i - j
+				}
+			}
+		}
+	}
+
+	if cnt == len(sumNums) {
+		return 0
+	}
+	return cnt
+}
+
+func main() {
+	data := []int{2, 3, 1, 2, 4, 3}
+	target := 11
+	res := minSubArrayLen(target, data)
+	fmt.Println(res)
+}
