a61ec16e87908d584787183c2b1878771cd0b63a
renpeng <renpeng@p1.com>
2024-11-17 15:52:46 +0800

    add

--- src/geekbang/数据结构与算法之美/search.go
+++ src/geekbang/数据结构与算法之美/search.go
@@ -24,3 +24,29 @@ func bsearch(a []int, value int) int {
 	}
 	return -1
 }
+
+//***************二分查找变形
+// 1. 查找第一个值等于给定值的元素
+
+func bsearch1(a []int, value int) int {
+	low := 0
+	high := len(a) - 1
+
+	for low <= high {
+		mid := low + (high-low)>>1
+
+		if a[mid] > value {
+			high = mid - 1
+		} else if a[mid] < value {
+			low = mid + 1
+		} else {
+			if (a[mid-1] != value) || mid == 0 {
+				return mid
+			} else {
+				high = mid - 1
+			}
+		}
+	}
+	return -1
+
+}
