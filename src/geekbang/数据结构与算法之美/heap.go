72fe42ab6ca903a2e54e446a6e3ffc3bbd38a1f1
renpeng <renpeng@p1.com>
2024-11-17 18:08:01 +0800

    add

--- src/geekbang/数据结构与算法之美/heap.go
+++ src/geekbang/数据结构与算法之美/heap.go
@@ -52,3 +52,43 @@ func (heap *Heap) heapify(a []int, n, i int) {
 func swap(a []int, i, j int) {
 	a[i], a[j] = a[j], a[i]
 }
+
+// 堆排序
+// 1. 建堆 第一种，按照传统方式一个一个插入建立堆。从前往后处理数组数据，并且每个数据插入堆中时，都是从下往上堆化
+//        第二种是从后往前处理数组，并且每个数据都是从上往下堆化。因为叶子节点往下堆化只能自己和自己比较，所以直接从最后一个非叶子节点开始堆化
+// 2. 排序 数组中的第一个元素就是堆顶，也就是最大元素，把他和最后一个元素交换，然后再进行堆化
+
+func buildHeap(a []int, n int) {
+	for i := n / 2; i >= 1; i-- {
+		heapify(a, n, i)
+	}
+
+}
+
+// 每个节点堆化的时间复杂度是O(logn)，则n/2 + 1 个节点堆化总时间O(nlogn)，但是实际上是O(n)，因为叶子节点不需要堆化，所以需要堆化的节点从倒数第二层开始，每个节点堆化的过程中，需要比较和交换的节点个数，跟这个节点的高度k成正比
+func heapify(a []int, n, i int) {
+	for {
+		maxPos := i
+		if i*2 < n && a[i] < a[i*2] {
+			maxPos = i * 2
+		}
+		if i*2+1 < n && a[i] < a[i*2+1] {
+			maxPos = i*2 + 1
+		}
+		if maxPos == i {
+			break
+		}
+		swap(a, i, maxPos)
+		i = maxPos
+	}
+}
+
+func heapSort(a []int, n int) {
+	buildHeap(a, n)
+	k := n
+	for k > 1 {
+		swap(a, 1, k)
+		k--
+		heapify(a, k, 1)
+	}
+}
