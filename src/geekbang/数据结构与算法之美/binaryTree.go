697a3ab545d80c1d2b12ffb9b658d05ebf52db79
renpeng <renpeng@p1.com>
2024-11-17 17:16:29 +0800

    add

--- src/geekbang/数据结构与算法之美/binaryTree.go
+++ src/geekbang/数据结构与算法之美/binaryTree.go
@@ -41,3 +41,94 @@ func postOrder(root *BinaryTreeNode) {
 }
 
 // 层序遍历TBD
+
+// 二叉查找树： 在树中的任意节点，其左子树中的每个节点的值，都小于这个节点的值，而右子树节点的值都大于这个节点的值
+// 支持动态数据集合的快速插入，删除和查找
+
+type BinarySearchTree struct {
+	Root *BinaryTreeNode
+}
+
+func (binarySearchTree *BinarySearchTree) Find(data int) *BinaryTreeNode {
+	p := binarySearchTree.Root
+	for p != nil {
+		if data < p.Val {
+			p = p.Left
+		} else if data > p.Val {
+			p = p.Right
+		} else {
+			return p
+		}
+	}
+	return nil
+}
+
+func (binarySearchTree *BinarySearchTree) Insert(data int) {
+	if binarySearchTree.Root == nil {
+		binarySearchTree.Root = &BinaryTreeNode{
+			Val: data,
+		}
+		return
+	}
+
+	p := binarySearchTree.Root
+	for p != nil {
+		if data > p.Val {
+			if p.Right == nil {
+				p.Right = &BinaryTreeNode{Val: data}
+				return
+			}
+			p = p.Right
+		} else {
+			if p.Left == nil {
+				p.Left = &BinaryTreeNode{Val: data}
+				return
+			}
+			p = p.Left
+		}
+	}
+}
+
+func (binarySearchTree *BinarySearchTree) Delete(data int) {
+	p := binarySearchTree.Root
+	var pp *BinaryTreeNode
+	for p != nil && p.Val != data {
+		pp = p
+		if data > p.Val {
+			p = p.Right
+		} else {
+			p = p.Left
+		}
+	}
+	if p == nil {
+		return
+	}
+
+	// 要删除的节点右2个子节点
+	if p.Left != nil && p.Right != nil { // 查找右子树中最小节点
+		minP := p.Right
+		minPP := p
+		for minP.Left != nil {
+			minPP = minP
+			minP = minP.Left
+		}
+		p.Val = minP.Val // 将minP的数据替换到p中
+		p = minP         // 下面就变成了删除minP了
+		pp = minPP
+	}
+
+	var child *BinaryTreeNode
+	if p.Left != nil {
+		child = p.Left
+	} else if p.Right != nil {
+		child = p.Right
+	}
+
+	if pp == nil {
+		binarySearchTree.Root = child
+	} else if pp.Left == p {
+		pp.Left = child
+	} else {
+		pp.Right = child
+	}
+}
