2bacda3eda9686a4b04ad4333ba378e6b4e4c7df
renpeng <renpeng@hellogroup.com>
2024-12-25 15:35:21 +0800

    add

--- src/leetcode/2024_1223~1229/300. 最长递增子序列.go
+++ src/leetcode/2024_1223~1229/300. 最长递增子序列.go
@@ -1,25 +1,29 @@
 package main
 
-import "math"
+import (
+	"math"
+)
 
 func lengthOfLIS(nums []int) int {
 	dp := make([]int, len(nums))
 	for i := 0; i < len(nums); i++ {
 		dp[i] = 1
 	}
+	max := 1
 	for i := 1; i < len(nums); i++ {
 		for j := 0; j < i; j++ {
 			if nums[i] > nums[j] {
 				dp[i] = int(math.Max(float64(dp[j]+1), float64(dp[i])))
+				if dp[i] > max {
+					max = dp[i]
+				}
 			}
 		}
 	}
 
-	max := 0
-	for _, v := range dp {
-		if v > max {
-			max = v
-		}
-	}
 	return max
+}
+
+func main() {
+	lengthOfLIS([]int{1, 3, 6, 7, 9, 4, 10, 5, 6})
 }
