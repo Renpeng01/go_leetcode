a1e4dbc0bd360a86b7d72a1a55fc7352c01dc8ff
renpeng <renpeng@p1.com>
2024-12-29 21:25:36 +0800

    add

---
+++ src/leetcode/2024_1223~1229/343. 整数拆分.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func integerBreak(n int) int {
+
+	if n == 0 {
+		return 0
+	}
+
+	if n == 1 {
+		return 0
+	}
+
+	if n == 2 {
+		return 1
+	}
+
+	dp := make([]int, n+1) // 对i进行拆分，得到最大的乘积为dp[i]
+
+	dp[1] = 0
+	dp[2] = 1
+
+	for i := 3; i <= n; i++ {
+		for j := 1; j < i; j++ {
+			dp[i] = max(j*dp[i-j], max(dp[i], j*(i-j)))
+		}
+	}
+
+	return dp[n]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	fmt.Println("res: ", integerBreak(10))
+}
