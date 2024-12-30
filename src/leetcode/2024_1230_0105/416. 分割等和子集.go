7d6fc51de08a12bcb102ab501d057f92a4e67433
renpeng <renpeng@hellogroup.com>
2024-12-30 16:59:19 +0800

    add

--- src/leetcode/2024_1230_0105/416. 分割等和子集.go
+++ src/leetcode/2024_1230_0105/416. 分割等和子集.go
@@ -3,18 +3,15 @@ package main
 // 给你一个 只包含正整数 的 非空 数组 nums 。请你判断是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。
 
 func canPartition(nums []int) bool {
-
 	sum := 0
 	for _, v := range nums {
 		sum += v
 	}
-
 	if sum%2 == 1 {
 		return false
 	}
 
 	target := sum / 2
-
 	dp := make([]int, target+1)
 	for i := 0; i < len(nums); i++ {
 		for j := target; j >= nums[i]; j-- {
