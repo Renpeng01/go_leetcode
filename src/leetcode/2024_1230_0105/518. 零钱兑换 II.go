03bef72f6d5589f6b1bdd8686adae22143140f6f
renpeng <renpeng@p1.com>
2025-01-01 15:10:45 +0800

    add

--- src/leetcode/2024_1230_0105/518. 零钱兑换 II.go
+++ src/leetcode/2024_1230_0105/518. 零钱兑换 II.go
@@ -7,7 +7,7 @@ func change(amount int, coins []int) int {
 	dp := make([]int, amount+1)
 	dp[0] = 1
 
-	// 得到的是组合
+	// 得到的是组合  (每次遍历一个物品，不会有2个物品以先后不同的顺序出现的问题)
 	for i := 0; i < len(coins); i++ { // 物品
 		for j := coins[i]; j <= amount; j++ { // 背包
 			dp[j] += dp[j-coins[i]]
