d571557c3feea89fa68c0ac80f9ca761708d0e06
renpeng <renpeng@p1.com>
2025-06-07 18:46:29 +0800

    add

--- src/面试经典 150 题/数组和字符串/88. 合并两个有序数组.go
+++ src/面试经典 150 题/数组和字符串/88. 合并两个有序数组.go
@@ -35,8 +35,36 @@ func merge(nums1 []int, m int, nums2 []int, n int) {
 	utils.PrintIntSlice(nums1)
 }
 
+// 从后往前遍历，将最大的放在后面，可以节省一个slice的临时变量
+func merge1(nums1 []int, m int, nums2 []int, n int) {
+	nums1Idx := m - 1
+	nums2Idx := n - 1
+
+	tail := m + n - 1
+	for nums1Idx >= 0 && nums2Idx >= 0 {
+		if nums1[nums1Idx] > nums2[nums2Idx] {
+			nums1[tail] = nums1[nums1Idx]
+			nums1Idx--
+		} else {
+			nums1[tail] = nums2[nums2Idx]
+			nums2Idx--
+		}
+		tail--
+	}
+
+	for k := nums1Idx; k >= 0; k-- {
+		nums1[tail] = nums1[k]
+		tail--
+
+	}
+	for k := nums2Idx; k >= 0; k-- {
+		nums1[tail] = nums2[k]
+		tail--
+	}
+}
+
 func main() {
 	num1 := []int{4, 5, 6, 0, 0, 0}
 	num2 := []int{1, 2, 3}
-	merge(num1, 3, num2, 3)
+	merge1(num1, 3, num2, 3)
 }
