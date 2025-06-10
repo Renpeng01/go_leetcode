ce1181f67d7c24a3676957c03169e391a77269fc
renpeng <renpeng@hellogroup.com>
2025-06-10 17:23:30 +0800

    add

--- src/面试经典 150 题/数组和字符串/122. 买卖股票的最佳时机 II.go
+++ src/面试经典 150 题/数组和字符串/122. 买卖股票的最佳时机 II.go
@@ -28,3 +28,22 @@ func max(a, b int) int {
 
 	return b
 }
+
+func maxProfit1(prices []int) int {
+	min := prices[0]
+
+	maxProfit := 0
+
+	for i := 1; i < len(prices); i++ {
+		if prices[i] < min {
+			min = prices[i]
+		} else {
+			if prices[i]-min > maxProfit {
+				maxProfit = prices[i] - min
+			}
+		}
+	}
+
+	return maxProfit
+
+}
