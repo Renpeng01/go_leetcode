d53583945f1b77ada8167880174112452651f3d4
renpeng <renpeng@hellogroup.com>
2025-06-13 11:34:00 +0800

    add

--- src/面试经典 150 题/数组和字符串/134. 加油站.go
+++ src/面试经典 150 题/数组和字符串/134. 加油站.go
@@ -21,3 +21,26 @@ func canCompleteCircuit(gas []int, cost []int) int {
 	}
 	return -1
 }
+
+func canCompleteCircuit1(gas []int, cost []int) int {
+	n := len(gas)
+	has := 0
+	for i := 0; i < n; {
+		has = 0
+		m := i
+		for j := 0; j < n; j++ {
+			if has+gas[m]-cost[m] >= 0 {
+				has = has + gas[m] - cost[m]
+				m = (m + 1) % n
+				if j == n-1 {
+					return i
+				}
+				continue
+			}
+			i = i + j + 1 // 如果i不到i，则从i + j + 1开始继续往后走
+			break
+		}
+
+	}
+	return -1
+}
