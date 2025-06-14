491321c77f35d2fac15cfc85d6fbcd11e69e06b0
renpeng <renpeng@p1.com>
2025-06-14 22:08:53 +0800

    add

--- src/面试经典 150 题/数组和字符串/42. 接雨水.go
+++ src/面试经典 150 题/数组和字符串/42. 接雨水.go
@@ -36,3 +36,32 @@ func main() {
 	res := trap(height)
 	fmt.Println(res)
 }
+
+func trap(height []int) int {
+
+	stack := make([]int, 0, len(height))
+	res := 0
+	for i, h := range height {
+		for len(stack) > 0 && h > height[stack[len(stack)-1]] {
+			top := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			if len(stack) <= 0 {
+				break
+			}
+
+			minH := min(height[stack[len(stack)-1]], h) - height[top]
+			with := i - 1 - stack[len(stack)-1]
+			res += minH * with
+
+		}
+		stack = append(stack, i)
+	}
+	return res
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
