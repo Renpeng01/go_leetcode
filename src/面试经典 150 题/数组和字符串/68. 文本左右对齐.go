93cea15ee6c075d4accdac34685b5efb0ec99ed5
renpeng <renpeng@hellogroup.com>
2025-06-16 10:40:18 +0800

    add

---
+++ src/面试经典 150 题/数组和字符串/68. 文本左右对齐.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func fullJustify(words []string, maxWidth int) []string {
+	splitStrs := make([][]string, 0, len(words))
+
+	totalLen := len(words[0])
+	tmp := make([]string, 0, 8)
+	tmp = append(tmp, words[0])
+	for i := 1; i < len(words); i++ {
+		if totalLen+len(words[i])+1 > 16 {
+			splitStrs = append(splitStrs, tmp)
+			tmp = make([]string, 0, 8)
+			tmp = append(tmp, words[i])
+			totalLen = len(words[i])
+		} else {
+			tmp = append(tmp, words[i])
+			totalLen += len(words[i]) + 1
+		}
+		if i == len(words)-1 {
+			splitStrs = append(splitStrs, tmp)
+		}
+	}
+
+	isLeft := false
+	res := make([]string, 0, len(splitStrs))
+	for _, strs := range splitStrs {
+		str := ""
+
+		if len(strs) == 1 {
+			str += strs[0] + strings.Repeat(" ", maxWidth-len(strs[0]))
+			isLeft = true
+		} else {
+			wordsTotalLen := 0
+			for _, clildStr := range strs {
+				wordsTotalLen += len(clildStr)
+			}
+			paddingCnt := maxWidth - wordsTotalLen
+			moreCnt := paddingCnt % (len(strs) - 1)
+			if moreCnt == 0 && !isLeft {
+				str = strings.Join(strs, strings.Repeat(" ", paddingCnt/(len(strs)-1)))
+			} else {
+				strs[0] += strings.Repeat(" ", moreCnt)
+				str = strings.Join(strs, " ") + strings.Repeat(" ", maxWidth-(len(strs)-1)-wordsTotalLen-moreCnt)
+				isLeft = true
+			}
+		}
+		res = append(res, str)
+	}
+	return res
+}
+
+func main() {
+	// words := []string{"This", "is", "an", "example", "of", "text", "justification."}
+	words := []string{"What", "must", "be", "acknowledgment", "shall", "be"}
+	res := fullJustify(words, 16)
+	// fmt.Println(strings.Join(res, ","))
+
+	for _, v := range res {
+		fmt.Println(v + ",")
+	}
+}
