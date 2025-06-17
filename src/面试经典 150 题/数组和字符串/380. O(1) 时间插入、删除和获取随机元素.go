37ef7e6a9cd5f5b2a3e720334cc52e1aadc5563a
renpeng <renpeng@hellogroup.com>
2025-06-17 10:09:16 +0800

    add

--- src/面试经典 150 题/数组和字符串/380. O(1) 时间插入、删除和获取随机元素.go
+++ src/面试经典 150 题/数组和字符串/380. O(1) 时间插入、删除和获取随机元素.go
@@ -1,14 +1,24 @@
 package main
 
 type RandomizedSet struct {
-	dataMap map[int]struct{}
-	data    []int
+	dataMap           map[int]*node
+	head              *node
+	currentNode       *node
+	returnCurrentNode *node
+}
+
+type node struct {
+	val  int
+	next *node
+	pre  *node
 }
 
 func Constructor() RandomizedSet {
 	e := RandomizedSet{}
-	e.dataMap = make(map[int]struct{}, 128)
-	e.data = make([]int, 0, 128)
+	e.dataMap = make(map[int]*node, 128)
+	e.head = new(node)
+	e.currentNode = e.head
+	e.returnCurrentNode = e.head
 	return e
 }
 
@@ -16,18 +26,33 @@ func (this *RandomizedSet) Insert(val int) bool {
 	if _, ok := this.dataMap[val]; ok {
 		return false
 	}
-	this.dataMap[val] = struct{}{}
-	this.data = append(this.data, val)
+	n := &node{
+		val: val,
+		pre: this.currentNode,
+	}
+	this.currentNode.next = n
+	this.dataMap[val] = n
+	return true
+
 }
 
 func (this *RandomizedSet) Remove(val int) bool {
 	if _, ok := this.dataMap[val]; ok {
+		deleteNode := this.dataMap[val]
 		delete(this.dataMap, val)
+		deleteNode.pre.next = deleteNode.next
+		deleteNode.next.pre = deleteNode.pre
 		return true
 	}
 	return false
 }
 
 func (this *RandomizedSet) GetRandom() int {
+	if this.returnCurrentNode.next != nil {
+		this.returnCurrentNode = this.returnCurrentNode.next
+	} else {
+		this.returnCurrentNode = this.head.next
+	}
 
+	return this.returnCurrentNode.val
 }
