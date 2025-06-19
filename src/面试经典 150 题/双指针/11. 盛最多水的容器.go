b691c38e628c381a4c974fec457d759bf6aaa782
renpeng <renpeng@hellogroup.com>
2025-06-19 19:01:44 +0800

    add

---
+++ src/面试经典 150 题/双指针/11. 盛最多水的容器.go
@@ -0,0 +1,38 @@
+package main
+
+func maxArea(height []int) int {
+	left := 0
+	right := len(height) - 1
+	maxArea := min(height[left], height[right]) * (right - left)
+
+	for left < right {
+		nextLeft := left + 1
+		nextRigth := right - 1
+		if nextLeft < right && height[left] < height[right] && height[nextLeft] > height[left] {
+			if min(height[nextLeft], height[right])*(right-nextLeft) > maxArea {
+				maxArea = min(height[nextLeft], height[right]) * (right - nextLeft)
+				left = nextLeft
+				continue
+			}
+		}
+
+		if nextRigth > left && height[right] < height[left] && height[nextRigth] > height[right] {
+			if min(height[nextRigth], height[left])*(nextRigth-left) > maxArea {
+				maxArea = min(height[nextRigth], height[left]) * (nextRigth - left)
+				right = nextRigth
+				continue
+			}
+		}
+		left++
+		right--
+	}
+
+	return maxArea
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
