c18ce53592cb6b27bfb64ba2945384fc00fa51c3
renpeng <renpeng@hellogroup.com>
2025-06-20 18:11:30 +0800

    add

--- src/面试经典 150 题/滑动窗口/3. 无重复字符的最长子串.go
+++ src/面试经典 150 题/滑动窗口/3. 无重复字符的最长子串.go
@@ -1,7 +1,10 @@
 package main
 
-func lengthOfLongestSubstring(s string) int {
+import (
+	"fmt"
+)
 
+func lengthOfLongestSubstring(s string) int {
 	if len(s) == 0 {
 		return 0
 	}
@@ -11,23 +14,34 @@ func lengthOfLongestSubstring(s string) int {
 	set[s[0]] = 0
 	begin := 0
 	i := 1
+	deleteInex := 0
 	for i < len(s) {
 		v, ok := set[s[i]]
 		if ok {
 			if (i - begin) > max {
 				max = i - begin
 			}
-			for j := 0; j <= v; j++ {
+			for j := deleteInex; j <= v; j++ {
 				delete(set, s[j])
 			}
+			deleteInex = v + 1
 			begin = v + 1
 		} else {
 			if i == len(s)-1 && (i-begin+1) > max {
 				max = i - begin + 1
 			}
 		}
+
 		set[s[i]] = i
 		i++
+
 	}
 	return max
+}
+
+func main() {
+	s := "abcabcbb"
+	res := lengthOfLongestSubstring(s)
+	fmt.Println(res)
+
 }
