94f0a2ca29227b764761808ce8bffc98293d0993
renpeng <renpeng@hellogroup.com>
2025-06-20 18:40:54 +0800

    add

---
+++ src/面试经典 150 题/滑动窗口/209. 长度最小的子数组.go
@@ -0,0 +1,43 @@
+package main
+
+import "math"
+
+func minSubArrayLen(target int, nums []int) int {
+	minLen := math.MaxInt
+
+	sum := 0
+
+	left := 0
+	right := 0
+
+	for left <= right && right < len(nums) {
+		sum += nums[right]
+		if sum >= target {
+			if right-left+1 < minLen {
+				minLen = right - left + 1
+			}
+			for left < right {
+				if (sum - nums[left]) >= target {
+
+					if right-(left+1)+1 < minLen {
+						minLen = right - (left + 1) + 1
+					}
+					sum -= nums[left]
+					left++
+					minLen--
+					continue
+				}
+				break
+			}
+			left++
+			right++
+		} else {
+			right++
+		}
+	}
+
+	if minLen == math.MaxInt {
+		return 0
+	}
+	return minLen
+}
