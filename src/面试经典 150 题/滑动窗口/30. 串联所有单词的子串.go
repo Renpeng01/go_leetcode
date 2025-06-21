fe3206f65bb0a702a83945f0d4059c8452b66e34
renpeng <renpeng@p1.com>
2025-06-21 14:27:55 +0800

    add

--- src/面试经典 150 题/滑动窗口/30. 串联所有单词的子串.go
+++ src/面试经典 150 题/滑动窗口/30. 串联所有单词的子串.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+// 超时
+// 179 / 182 个通过的测试用例
 func findSubstring(s string, words []string) []int {
 	res := make([]int, 0, 8)
 	step := len(words[0])
@@ -16,11 +18,13 @@ func findSubstring(s string, words []string) []int {
 	}
 	// fmt.Printf("wordSet: %+v, subStrLen: %+v\n", wordSet, subStrLen)
 
-	for i := 0; i < len(s)-subStrLen; i++ {
+	for i := 0; i <= len(s)-subStrLen; i++ {
+		// fmt.Printf("wordSet: %+v\n", wordSet)
+		// fmt.Printf("new turn i: %+v\n", i)
 		revertWords := make([]string, 0, len(words))
 		for b := 0; b <= subStrLen-step; b += step {
 			k := s[i+b : i+b+step]
-			// fmt.Printf("i: %+v b: %+v, step: %+v, (i+b): %+v,(i+b+step): %+v, key: %+v\n", i, b, step, i+b, i+b+step, k)
+			fmt.Printf("i: %+v b: %+v, step: %+v, (i+b): %+v,(i+b+step): %+v, key: %+v\n", i, b, step, i+b, i+b+step, k)
 			if wordSet[k] > 0 {
 				wordSet[k]--
 				revertWords = append(revertWords, k)
@@ -39,8 +43,10 @@ func findSubstring(s string, words []string) []int {
 }
 
 func main() {
-	s := "barfoothefoobarman"
-	words := []string{"foo", "bar"}
+	// s := "barfoothefoobarman"
+	// words := []string{"foo", "bar"}
+	s := "wordgoodgoodgoodbestword"
+	words := []string{"word", "good", "best", "good"}
 
 	res := findSubstring(s, words)
 	fmt.Printf("res: %+v\n", res)
