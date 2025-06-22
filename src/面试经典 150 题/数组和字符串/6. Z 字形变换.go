00c86b12aa48338b97ae4ba7d45925c5d5f52aba
renpeng <renpeng@p1.com>
2025-06-22 17:33:00 +0800

    ad

--- src/面试经典 150 题/数组和字符串/6. Z 字形变换.go
+++ src/面试经典 150 题/数组和字符串/6. Z 字形变换.go
@@ -1,27 +1,29 @@
 package main
 
-func convert(s string, numRows int) string {
-	col := (len(s) / (numRows + numRows - 2)) + 1
+import "fmt"
 
+func convert(s string, numRows int) string {
+	col := ((len(s) / (numRows + numRows - 2)) + 1) * (numRows - 1)
 	table := make([][]byte, numRows)
 	for i := 0; i < numRows; i++ {
 		table[i] = make([]byte, col)
 	}
 
+	fmt.Printf("col: %+v\n", col)
 	colStep := 0
-	rowStep := 0
 	for i := 0; i < len(s); i++ {
-
-		if i/(numRows+numRows-2) <= numRows-1 {
-			table[i/(numRows+numRows-2)+rowStep][colStep] = s[i]
+		if i%(numRows+numRows-2) == 0 {
+			colStep = (i / (numRows + numRows - 2)) * (numRows - 1)
+		}
+		if i%(numRows+numRows-2) <= numRows-1 {
+			fmt.Printf("11111111,i: %+v table[%+v][%+v]\n", i, i%(numRows+numRows-2), colStep)
+			table[i%(numRows+numRows-2)][colStep] = s[i]
 		} else {
-			table[numRows-1-i/(numRows+numRows-2)-(numRows-1)][i/(numRows+numRows-2)-(numRows-1)] = s[i]
+			fmt.Printf("22222222,i: %+v table[%+v][%+v]\n", i, numRows-1-(i%(numRows+numRows-2)-(numRows-1)), i%(numRows+numRows-2)-(numRows-1))
+			table[numRows-1-(i%(numRows+numRows-2)-(numRows-1))][i%(numRows+numRows-2)-(numRows-1)] = s[i]
+
 		}
 
-		if (i+1)%(numRows+numRows-2) == 0 {
-			colStep = i * (numRows - 1)
-			rowStep = i * (numRows - 1)
-		}
 	}
 
 	res := ""
@@ -33,4 +35,12 @@ func convert(s string, numRows int) string {
 		}
 	}
 	return res
+}
+
+func main() {
+	s := "PAYPALISHIRING"
+	numRows := 3
+
+	res := convert(s, numRows)
+	fmt.Printf("res: %+v", res)
 }
