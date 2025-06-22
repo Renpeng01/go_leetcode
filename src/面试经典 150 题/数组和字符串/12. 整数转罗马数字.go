4111e981ddc4ecaa827207a11ad0293bde955694
renpeng <renpeng@p1.com>
2025-06-22 19:02:25 +0800

    add

--- src/面试经典 150 题/数组和字符串/12. 整数转罗马数字.go
+++ src/面试经典 150 题/数组和字符串/12. 整数转罗马数字.go
@@ -1,14 +1,78 @@
 package main
 
-func intToRoman(num int) string {
+import (
+	"fmt"
+	"sort"
+)
 
-	rel := map[int]byte{
-		1:    'I',
-		5:    'V',
-		10:   'X',
-		50:   'L',
-		100:  'C',
-		500:  'D',
-		1000: 'M',
+func intToRoman(num int) string {
+	rel := map[int]string{
+		1:    "I",
+		5:    "V",
+		10:   "X",
+		50:   "L",
+		100:  "C",
+		500:  "D",
+		1000: "M",
 	}
+
+	keys := make([]int, 0, len(rel))
+	for k := range rel {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+	res := ""
+	fmt.Println(keys)
+
+	for i := len(keys) - 1; i >= 0; i-- {
+		fmt.Printf("i: %+v, key: %+v, num: %+v, num/keys[i]: %+v\n", i, keys[i], num, num/keys[i])
+		if num/keys[i] > 0 {
+			if num/keys[i] == 4 {
+				if i == 0 {
+					res += "VI"
+				} else if i == 2 {
+					res += "XL"
+				} else if i == 4 {
+					res += "CD"
+				} else {
+					for j := 0; j < num/keys[i]; j++ {
+						res += rel[keys[i]]
+					}
+				}
+			} else if num/keys[i] == 9 || num == 9 {
+				if i == 0 || i == 1 {
+					res += "IX"
+					break
+				} else if i == 2 {
+					res += "XC"
+				} else if i == 4 {
+					res += "CM"
+				} else {
+					for j := 0; j < num/keys[i]; j++ {
+						res += rel[keys[i]]
+					}
+				}
+
+			} else {
+				// fmt.Printf("cnt: %+v,relVal: %+v\n", num/keys[i], rel[keys[i]])
+				for j := 0; j < num/keys[i]; j++ {
+					res += rel[keys[i]]
+				}
+
+			}
+
+		}
+		num = num - num/keys[i]*keys[i]
+	}
+	return res
+}
+
+// MMMDCCXXXXIX
+// MMMDCCXLIX
+// MMMDCCXLIX
+
+func main() {
+	num := 1900
+	res := intToRoman(num)
+	fmt.Println(res)
 }
