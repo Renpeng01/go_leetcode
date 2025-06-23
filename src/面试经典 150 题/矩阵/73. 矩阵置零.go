81dc9dd0f17cad336ba1a22e27b6aa5851f319fd
renpeng <renpeng@hellogroup.com>
2025-06-23 13:59:23 +0800

    add

---
+++ src/面试经典 150 题/矩阵/73. 矩阵置零.go
@@ -0,0 +1,31 @@
+package main
+
+// 通过 但性能不佳
+func setZeroes(matrix [][]int) {
+
+	res := make([][]int, 0, len(matrix)*len(matrix[0]))
+	for i := 0; i < len(matrix); i++ {
+		for j := 0; j < len(matrix[0]); j++ {
+			if matrix[i][j] == 0 {
+
+				// row set 0
+				for c := 0; c < len(matrix[0]); c++ {
+					// matrix[i][c] = 0
+					res = append(res, []int{i, c})
+				}
+				// rol set 0
+				for r := 0; r < len(matrix); r++ {
+					// matrix[r][j] = 0
+					res = append(res, []int{r, j})
+				}
+
+			}
+		}
+	}
+
+	for _, v := range res {
+		matrix[v[0]][v[1]] = 0
+	}
+}
+
+// 使用常量空间
