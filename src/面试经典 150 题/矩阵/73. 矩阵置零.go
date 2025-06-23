d5d4746cc4919f0d34c2740796f23d4614ed0f36
renpeng <renpeng@hellogroup.com>
2025-06-23 16:46:22 +0800

    add

--- src/面试经典 150 题/矩阵/73. 矩阵置零.go
+++ src/面试经典 150 题/矩阵/73. 矩阵置零.go
@@ -1,7 +1,7 @@
 package main
 
 // 通过 但性能不佳
-func setZeroes(matrix [][]int) {
+func setZeroes1(matrix [][]int) {
 
 	res := make([][]int, 0, len(matrix)*len(matrix[0]))
 	for i := 0; i < len(matrix); i++ {
@@ -25,6 +25,28 @@ func setZeroes(matrix [][]int) {
 
 	for _, v := range res {
 		matrix[v[0]][v[1]] = 0
+	}
+}
+
+func setZeroes(matrix [][]int) {
+	rows := make([]bool, len(matrix))
+	cols := make([]bool, len(matrix[0]))
+	for i := 0; i < len(matrix); i++ {
+		for j := 0; j < len(matrix[0]); j++ {
+			if matrix[i][j] == 0 {
+				rows[i] = true
+				cols[j] = true
+
+			}
+		}
+	}
+
+	for i := 0; i < len(matrix); i++ {
+		for j := 0; j < len(matrix[0]); j++ {
+			if rows[i] || cols[j] {
+				matrix[i][j] = 0
+			}
+		}
 	}
 }
 
