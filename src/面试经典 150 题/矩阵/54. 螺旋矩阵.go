e19b64f9428c6ecc7e78d6318e4bac86d146c749
renpeng <renpeng@hellogroup.com>
2025-06-25 10:57:03 +0800

    add

---
+++ src/面试经典 150 题/矩阵/54. 螺旋矩阵.go
@@ -0,0 +1,61 @@
+package main
+
+import "fmt"
+
+func spiralOrder(matrix [][]int) []int {
+	if len(matrix) <= 1 {
+		return matrix[0]
+	}
+
+	row := len(matrix)
+	col := len(matrix[0])
+
+	eleCnt := row * col
+	res := make([]int, 0, eleCnt)
+
+	round := 0
+	cnt := 0
+	for cnt < eleCnt {
+		// fmt.Printf("cnt: %+v, eleCnt: %+v\n", cnt, eleCnt)
+		// 上
+		for i := round; i < col-round-1; i++ {
+			if cnt > eleCnt {
+				break
+			}
+			res = append(res, matrix[round][i])
+			cnt++
+		}
+		// 右
+		for i := round; i < row-round-1; i++ {
+			if cnt > eleCnt {
+				break
+			}
+			res = append(res, matrix[i][col-round-1])
+			cnt++
+		}
+		// 下
+		for i := col - round - 1; i >= round+1; i-- {
+			if cnt > eleCnt {
+				break
+			}
+			res = append(res, matrix[row-round-1][i])
+			cnt++
+		}
+		// 左
+		for i := row - round - 1; i >= round+1; i-- {
+			if cnt > eleCnt {
+				break
+			}
+			res = append(res, matrix[i][round])
+			cnt++
+		}
+		round++
+	}
+	return res
+}
+
+func main() {
+	matrix := [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
+	res := spiralOrder(matrix)
+	fmt.Println(res)
+}
