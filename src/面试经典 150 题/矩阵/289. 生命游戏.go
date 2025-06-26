2507f395fd446bae8a58a037995ff9325bc7d1d4
renpeng <renpeng@hellogroup.com>
2025-06-26 10:25:35 +0800

    add

---
+++ src/面试经典 150 题/矩阵/289. 生命游戏.go
@@ -0,0 +1,58 @@
+package main
+
+func gameOfLife(board [][]int) {
+
+	offsets := [][]int{
+		{-1, -1}, {-1, 0}, {1, 1},
+		{0, -1}, {0, 1},
+		{1, -1}, {1, 0}, {1, 1},
+	}
+
+	newBard := make([][]int, len(board))
+	for i := 0; i < len(newBard); i++ {
+		newBard[i] = make([]int, len(board[0]))
+	}
+
+	for i := 0; i < len(board); i++ {
+		for j := 0; j < len(board[0]); j++ {
+			c0 := 0
+			c1 := 0
+			for _, v := range offsets {
+				if i+v[0] >= 0 && i+v[0] < len(board) && j+v[1] >= 0 && j+v[1] < len(board[0]) {
+					if board[i+v[0]][j+v[1]] == 0 {
+						c0++
+					} else {
+						c1++
+					}
+				}
+			}
+
+			v := board[i][j]
+			if board[i][j] == 1 {
+				if c1 < 2 {
+					v = 0
+				} else if c1 >= 2 && c1 <= 3 {
+					v = 1
+				} else {
+					v = 0
+				}
+			} else {
+				if c1 == 3 {
+					v = 1
+				}
+			}
+			newBard[i][j] = v
+		}
+	}
+
+	for i := 0; i < len(board); i++ {
+		for j := 0; j < len(board[0]); j++ {
+			board[i][j] = newBard[i][j]
+		}
+	}
+}
+
+func main() {
+	board := [][]int{{0, 1, 0}, {0, 0, 1}, {1, 1, 1}, {0, 0, 0}}
+	gameOfLife(board)
+}
