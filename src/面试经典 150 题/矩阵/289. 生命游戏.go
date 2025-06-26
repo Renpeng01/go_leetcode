f9966417b5fc15391716cfadc8fee976d673ce08
renpeng <renpeng@hellogroup.com>
2025-06-26 10:46:26 +0800

    add

--- src/面试经典 150 题/矩阵/289. 生命游戏.go
+++ src/面试经典 150 题/矩阵/289. 生命游戏.go
@@ -1,6 +1,6 @@
 package main
 
-func gameOfLife(board [][]int) {
+func gameOfLife1(board [][]int) {
 
 	offsets := [][]int{
 		{-1, -1}, {-1, 0}, {-1, 1},
@@ -48,6 +48,57 @@ func gameOfLife(board [][]int) {
 	for i := 0; i < len(board); i++ {
 		for j := 0; j < len(board[0]); j++ {
 			board[i][j] = newBard[i][j]
+		}
+	}
+}
+
+func gameOfLife(board [][]int) {
+
+	offsets := [][]int{
+		{-1, -1}, {-1, 0}, {-1, 1},
+		{0, -1}, {0, 1},
+		{1, -1}, {1, 0}, {1, 1},
+	}
+
+	// 0->1 2
+	// 1->0 3
+	for i := 0; i < len(board); i++ {
+		for j := 0; j < len(board[0]); j++ {
+			c0 := 0
+			c1 := 0
+			for _, v := range offsets {
+				if i+v[0] >= 0 && i+v[0] < len(board) && j+v[1] >= 0 && j+v[1] < len(board[0]) {
+					if board[i+v[0]][j+v[1]] == 0 || board[i+v[0]][j+v[1]] == 2 {
+						c0++
+					} else {
+						c1++
+					}
+				}
+			}
+
+			v := board[i][j]
+			if board[i][j] == 1 {
+				if c1 < 2 {
+					v = 3
+				} else if c1 > 3 {
+					v = 3
+				}
+			} else {
+				if c1 == 3 {
+					v = 2
+				}
+			}
+			board[i][j] = v
+		}
+	}
+
+	for i := 0; i < len(board); i++ {
+		for j := 0; j < len(board[0]); j++ {
+			if board[i][j] == 2 {
+				board[i][j] = 1
+			} else if board[i][j] == 3 {
+				board[i][j] = 0
+			}
 		}
 	}
 }
