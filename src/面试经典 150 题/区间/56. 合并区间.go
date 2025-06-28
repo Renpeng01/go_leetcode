a1557178b5ad7f20d204e1cb05badbad211e1f10
renpeng <renpeng@p1.com>
2025-06-28 21:51:49 +0800

    add

---
+++ src/面试经典 150 题/区间/56. 合并区间.go
@@ -0,0 +1,39 @@
+package main
+
+import "sort"
+
+func merge(intervals [][]int) [][]int {
+
+	sort.SliceStable(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+
+	res := make([][]int, 0, len(intervals))
+	for i := 0; i < len(intervals); {
+
+		item := make([]int, 2)
+		item[0] = intervals[i][0]
+		item[1] = intervals[i][1]
+
+		e := i
+		for j := i + 1; j < len(intervals); j++ {
+			if intervals[i][1] < intervals[j][0] {
+				break
+			}
+			item[1] = max(intervals[j][1], intervals[i][1])
+			e = j
+		}
+
+		res = append(res, item)
+		i = e + 1
+	}
+	return res
+
+}
+
+func max(i, j int) int {
+	if i > j {
+		return i
+	}
+	return j
+}
