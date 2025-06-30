c173207f282a791a8bfb605c0b79fdfc47104b42
renpeng <renpeng@hellogroup.com>
2025-06-30 13:31:54 +0800

    add

--- src/面试经典 150 题/区间/57. 插入区间.go
+++ src/面试经典 150 题/区间/57. 插入区间.go
@@ -4,60 +4,6 @@ import (
 	"fmt"
 )
 
-func insert1(intervals [][]int, newInterval []int) [][]int {
-
-	if len(intervals) == 0 {
-		return [][]int{newInterval}
-	}
-
-	res := make([][]int, 0, len(intervals))
-	start := -1
-	for i := 0; i < len(intervals); i++ {
-		if intervals[i][1] < newInterval[0] {
-			res = append(res, intervals[i])
-			start = i
-		} else if intervals[i][0] > newInterval[1] {
-			// res = append(res, newInterval)
-			// res = append(res, intervals[i])
-			// start = i
-			// break
-			res = append(res, newInterval)
-			for j := i; j < len(intervals); j++ {
-				res = append(res, intervals[j])
-			}
-			return res
-		}
-		break
-	}
-
-	fmt.Printf("start: %+v,res: %+v\n", start, res)
-	item := []int{newInterval[0], newInterval[1]}
-	fmt.Printf("item: %+v\n", item)
-	otherStart := start
-	for i := start + 1; i < len(intervals); i++ {
-		if item[1] < intervals[i][0] {
-			otherStart = i - 1
-			break
-		}
-		if item[1] >= intervals[i][0] {
-			item[0] = min(item[0], intervals[i][0])
-			item[1] = max(item[1], intervals[i][1])
-			otherStart = i
-			continue
-		}
-		break
-	}
-
-	res = append(res, item)
-	fmt.Printf("otherStart: %+v,res: %+v\n", otherStart, res)
-
-	for i := otherStart + 1; i < len(intervals); i++ {
-		res = append(res, intervals[i])
-	}
-	fmt.Println(res)
-	return res
-}
-
 func max(i, j int) int {
 	if i > j {
 		return i
