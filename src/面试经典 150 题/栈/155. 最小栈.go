33b3b8597822387065364c7b04d1e793af0c2b59
renpeng <renpeng@hellogroup.com>
2025-06-30 14:45:47 +0800

    add

--- src/面试经典 150 题/栈/155. 最小栈.go
+++ src/面试经典 150 题/栈/155. 最小栈.go
@@ -2,28 +2,28 @@ package main
 
 import "sort"
 
-type MinStack struct {
+type MinStack1 struct {
 	data       []int
 	sortedData []int
 }
 
-func Constructor() MinStack {
+func Constructor1() MinStack1 {
 	data := make([]int, 0, 256)
 	sortedData := make([]int, 0, 256)
-	stack := MinStack{
+	stack := MinStack1{
 		data:       data,
 		sortedData: sortedData,
 	}
 	return stack
 }
 
-func (this *MinStack) Push(val int) {
+func (this *MinStack1) Push(val int) {
 	this.data = append(this.data, val)
 	this.sortedData = append(this.sortedData, val)
 	sort.Ints(this.sortedData)
 }
 
-func (this *MinStack) Pop() {
+func (this *MinStack1) Pop() {
 	deleteVal := this.data[len(this.data)-1]
 	this.data = this.data[:len(this.data)-1]
 	for i := len(this.sortedData) - 1; i >= 0; i-- {
@@ -44,23 +44,66 @@ func (this *MinStack) Pop() {
 	}
 }
 
-func (this *MinStack) Top() int {
+func (this *MinStack1) Top() int {
 	if this != nil || len(this.data) > 0 {
 		return this.data[len(this.data)-1]
 	}
 	return -1
 }
 
-func (this *MinStack) GetMin() int {
+func (this *MinStack1) GetMin() int {
 	return this.sortedData[0]
 
 }
 
 /**
- * Your MinStack object will be instantiated and called as such:
+ * Your MinStack1 object will be instantiated and called as such:
  * obj := Constructor();
  * obj.Push(val);
  * obj.Pop();
  * param_3 := obj.Top();
  * param_4 := obj.GetMin();
  */
+
+type MinStack struct {
+	data     []int
+	minStack []int
+}
+
+func Constructor() MinStack {
+	data := make([]int, 0, 256)
+	minStack := make([]int, 0, 256)
+	stack := MinStack{
+		data:     data,
+		minStack: minStack,
+	}
+	return stack
+}
+
+func (this *MinStack) Push(val int) {
+
+	this.data = append(this.data, val)
+	if len(this.data) == 1 {
+		this.minStack = append(this.minStack, val)
+		return
+	}
+
+	if val < this.minStack[len(this.minStack)-1] {
+		this.minStack = append(this.minStack, val)
+	} else {
+		this.minStack = append(this.minStack, this.minStack[len(this.minStack)-1])
+	}
+}
+
+func (this *MinStack) Pop() {
+	this.data = this.data[:len(this.data)-1]
+	this.minStack = this.data[:len(this.minStack)-1]
+}
+
+func (this *MinStack) Top() int {
+	return this.data[len(this.data)-1]
+}
+
+func (this *MinStack) GetMin() int {
+	return this.minStack[len(this.minStack)-1]
+}
