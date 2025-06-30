e53e73d678f0fe9706d1c67fcce4cdddddbad1dc
renpeng <renpeng@hellogroup.com>
2025-06-30 11:50:18 +0800

    add

--- src/面试经典 150 题/区间/57. 插入区间.go
+++ src/面试经典 150 题/区间/57. 插入区间.go
@@ -73,8 +73,11 @@ func main() {
 	// intervals := [][]int{{1, 5}}
 	// newInterval := []int{2, 3}
 
-	intervals := [][]int{{1, 3}, {6, 9}}
-	newInterval := []int{2, 5}
+	// intervals := [][]int{{1, 3}, {6, 9}}
+	// newInterval := []int{2, 5}
+
+	intervals := [][]int{{1, 5}}
+	newInterval := []int{0, 0}
 
 	res := insert(intervals, newInterval)
 	fmt.Println(res)
