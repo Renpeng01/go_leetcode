1b79a9b0a20f98a9e662ac2e6131805124a303ae
renpeng <renpeng@hellogroup.com>
2025-07-03 10:29:48 +0800

    add

--- src/面试经典 150 题/链表/86. 分隔链表.go
+++ src/面试经典 150 题/链表/86. 分隔链表.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type ListNode struct {
 	Val  int
@@ -24,7 +26,7 @@ func partition(head *ListNode, x int) *ListNode {
 			mNext.Next = head
 			mNext = mNext.Next
 		} else {
-			if mNext.Next != nil {
+			if next.Next != nil || pre.Next == nil {
 				mNext.Next = head
 				mNext = mNext.Next
 			} else {
@@ -32,21 +34,25 @@ func partition(head *ListNode, x int) *ListNode {
 				mPre = mPre.Next
 			}
 		}
+
+		h := head
 		head = head.Next
+		h.Next = nil
+
 	}
 
 	// log
-	logPre := pre
-	logNext := next
-	for logPre.Next != nil {
-		fmt.Println("pre:", logPre.Next.Val)
-		logPre = logPre.Next
-	}
+	// logPre := pre
+	// logNext := next
+	// for logPre.Next != nil {
+	// 	fmt.Println("pre:", logPre.Next.Val)
+	// 	logPre = logPre.Next
+	// }
 
-	for logNext.Next != nil {
-		fmt.Println("next:", logNext.Next.Val)
-		logNext = logNext.Next
-	}
+	// for logNext.Next != nil {
+	// 	fmt.Println("next:", logNext.Next.Val)
+	// 	logNext = logNext.Next
+	// }
 
 	if pre.Next == nil {
 		return pre.Next
@@ -61,22 +67,32 @@ func partition(head *ListNode, x int) *ListNode {
 }
 
 func main() {
-	node1 := &ListNode{Val: 1, Next: nil}
-	node2 := &ListNode{Val: 4, Next: nil}
-	node3 := &ListNode{Val: 3, Next: nil}
-	node4 := &ListNode{Val: 2, Next: nil}
-	node5 := &ListNode{Val: 5, Next: nil}
-	node6 := &ListNode{Val: 2, Next: nil}
+	// node1 := &ListNode{Val: 1, Next: nil}
+	// node2 := &ListNode{Val: 4, Next: nil}
+	// node3 := &ListNode{Val: 3, Next: nil}
+	// node4 := &ListNode{Val: 2, Next: nil}
+	// node5 := &ListNode{Val: 5, Next: nil}
+	// node6 := &ListNode{Val: 2, Next: nil}
 
+	// node1.Next = node2
+	// node2.Next = node3
+	// node3.Next = node4
+	// node4.Next = node5
+	// node5.Next = node6
+
+	node1 := &ListNode{Val: 2, Next: nil}
+	node2 := &ListNode{Val: 1, Next: nil}
 	node1.Next = node2
-	node2.Next = node3
-	node3.Next = node4
-	node4.Next = node5
-	node5.Next = node6
+
+	mHead := node1
+	for mHead != nil {
+		fmt.Println("origin: ", mHead.Val)
+		mHead = mHead.Next
+	}
 
 	// partition(node1, 3)
 
-	res := partition(node1, 3)
+	res := partition(node1, 2)
 	i := 10
 	for res != nil && i > 0 {
 		fmt.Println("res:", res.Val)
