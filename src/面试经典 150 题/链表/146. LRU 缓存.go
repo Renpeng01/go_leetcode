807fb6f526dd90c591b14719a07875e384e25906
renpeng <renpeng@hellogroup.com>
2025-07-08 09:04:26 +0800

    add

--- src/面试经典 150 题/链表/146. LRU 缓存.go
+++ src/面试经典 150 题/链表/146. LRU 缓存.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 type listNode struct {
 	val  int
 	key  int
@@ -21,9 +19,9 @@ func Constructor(capacity int) LRUCache {
 	head := &listNode{}
 	return LRUCache{
 		head:     head,
+		tail:     head,
 		m:        make(map[int]*listNode, capacity),
 		capacity: capacity,
-		tail:     head,
 	}
 }
 
@@ -32,144 +30,62 @@ func (this *LRUCache) Get(key int) int {
 	if !ok {
 		return -1
 	}
-
-	if this.head.next != node {
-
-		if this.tail == node {
-			this.tail = this.tail.pre
-
-			node.pre.next = nil
-
-			node.next = this.head.next
-			this.head.next.pre = node
-			this.head.next = node
-			node.pre = this.head
-
-		} else {
-			node.pre.next = node.next
-			node.next.pre = node.pre
-
-			node.next = this.head.next
-			this.head.next.pre = node
-
-			this.head.next = node
-			node.pre = this.head
-		}
-	}
-	return node.val
+	res := node.val
+	this.touch(node)
+	return res
 }
 
-func (this *LRUCache) Put(key int, value int) {
+func (this *LRUCache) touch(node *listNode) {
 
-	node, ok := this.m[key]
-	if !ok {
-		newNode := &listNode{
-			val: value,
-			key: key,
-		}
-		if this.head.next == nil {
-			this.head.next = newNode
-			newNode.pre = this.head
-			this.tail = newNode
-		} else {
-			newNode.next = this.head.next
-			this.head.next.pre = newNode
-			this.head.next = newNode
-			newNode.pre = this.head
-		}
-		if len(this.m) >= this.capacity {
-
-			delete(this.m, this.tail.key)
-			this.tail = this.tail.pre
-			this.tail.next = nil
-		}
-		this.m[key] = newNode
-
+	if this.head.next == node {
+		return
+	}
+	if this.tail == node {
+		this.tail = this.tail.pre
+		this.tail.next = nil
 	} else {
-		node.val = value
-		if node == this.head.next {
-			return
-		}
-
-		if node == this.tail {
-			this.tail = node.pre
-		}
 		node.pre.next = node.next
 		node.next.pre = node.pre
 
-		node.next = this.head.next
-		this.head.next.pre = node
-		node.pre = this.head
-		this.head.next = node
-
 	}
+	node.next = this.head.next
+	this.head.next.pre = node
+	this.head.next = node
+	node.pre = this.head
+
 }
 
-func main() {
+func (this *LRUCache) Put(key int, value int) {
+	node, ok := this.m[key]
+	if ok {
+		node.val = value
+	} else {
+		if len(this.m) >= this.capacity {
+			targetNode := this.tail
+			targetNode.key = key
+			targetNode.val = value
+			this.touch(targetNode)
 
-	lru := Constructor(2)
-	lru.Put(1, 1)
-	lru.Put(2, 2)
+		} else {
+			newNode := &listNode{
+				key: key,
+				val: value,
+			}
 
-	// head := lru.head
-	// tail := lru.tail
+			if this.head.next == nil {
+				this.tail = newNode
+				this.head.next = newNode
+				newNode.pre = this.head
+			} else {
+				newNode.next = this.head.next
+				this.head.next.pre = newNode
+				this.head.next = newNode
+				newNode.pre = this.head
 
-	// fmt.Println("head: ", head.key, head.val)
-	// fmt.Println("tail: ", tail.key, tail.val)
-
-	// cur := head.next
-	// for cur != nil {
-	// 	fmt.Println(cur.key, cur.val)
-	// 	cur = cur.next
-	// }
-
-	// fmt.Println("m: ", lru.m)
-	// fmt.Println("capacity: ", lru.capacity)
-
-	res := lru.Get(1)
-	fmt.Println("get(1):", res)
-
-	// head := lru.head
-	// tail := lru.tail
-
-	// fmt.Println("head: ", head.key, head.val)
-	// fmt.Println("tail: ", tail.key, tail.val)
-
-	// cur := head.next
-	// for cur != nil {
-	// 	fmt.Println(cur.key, cur.val)
-	// 	cur = cur.next
-	// }
-
-	// fmt.Println("m: ", lru.m)
-	// fmt.Println("capacity: ", lru.capacity)
-
-	lru.Put(3, 3)
-
-	head := lru.head
-	tail := lru.tail
-
-	fmt.Println("head: ", head.key, head.val)
-	fmt.Println("tail: ", tail.key, tail.val)
-
-	cur := head.next
-	for cur != nil {
-		fmt.Println(cur.key, cur.val)
-		cur = cur.next
+				this.tail = this.tail.pre
+				this.tail.next = nil
+			}
+			this.m[key] = newNode
+		}
 	}
-
-	fmt.Println("m: ", lru.m)
-	fmt.Println("capacity: ", lru.capacity)
-	res = lru.Get(2)
-	fmt.Println("get(2):", res)
-	lru.Put(4, 4)
-	res = lru.Get(1)
-	fmt.Println("get(1):", res)
-	res = lru.Get(3)
-	fmt.Println("get(3):", res)
-	res = lru.Get(4)
-	fmt.Println("get(4):", res)
 }
-
-// "LRUCache","put","put","put","put","get","get"]
-// [[2],[2,1],[1,1],[2,3],[4,1],[1],[2]]
