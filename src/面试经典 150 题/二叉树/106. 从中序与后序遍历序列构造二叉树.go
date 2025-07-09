061e2226ad19fb46cb86339a59a55f09badefae7
renpeng <renpeng@hellogroup.com>
2025-07-09 09:44:01 +0800

    add

---
+++ src/面试经典 150 题/二叉树/106. 从中序与后序遍历序列构造二叉树.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func buildTree(inorder []int, postorder []int) *TreeNode {
+	if len(postorder) == 0 {
+		return nil
+	}
+
+	root := &TreeNode{
+		Val: postorder[len(postorder)-1],
+	}
+
+	rootIndex := 0
+	for i, v := range inorder {
+		if v == root.Val {
+			rootIndex = i
+			break
+		}
+	}
+	leftInOrder := inorder[0:rootIndex]
+	rightInOrder := inorder[rootIndex+1:]
+
+	fmt.Println("leftInOrder:", leftInOrder, "rightInOrder:", rightInOrder)
+
+	leftPostOrder := postorder[0:len(leftInOrder)]
+	rightPostOrder := postorder[len(leftInOrder):len(postorder)]
+
+	fmt.Println("leftPostOrder:", leftPostOrder, "rightPostOrder:", rightPostOrder)
+
+	root.Left = buildTree(leftInOrder, leftPostOrder)
+	root.Right = buildTree(rightInOrder, rightPostOrder)
+	return root
+
+}
