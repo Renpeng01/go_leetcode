928b2226988e9b49fe3ab18a008a48de0b001748
renpeng <renpeng@hellogroup.com>
2025-07-11 09:45:25 +0800

    add

---
+++ src/leetcode/面试经典 150 题/二叉树/173. 二叉搜索树迭代器.go
@@ -0,0 +1,22 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+type BSTIterator struct {
+}
+
+func Constructor(root *TreeNode) BSTIterator {
+
+}
+
+func (this *BSTIterator) Next() int {
+
+}
+
+func (this *BSTIterator) HasNext() bool {
+
+}
