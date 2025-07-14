ee75296974b62f57bf6cdbf843a518db069ce012
renpeng <renpeng@hellogroup.com>
2025-07-14 16:44:10 +0800

    add

---
+++ src/leetcode/面试经典 150 题/二叉搜索树/98. 验证二叉搜索树.go
@@ -0,0 +1,35 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+var arr []int
+
+func isValidBST(root *TreeNode) bool {
+	arr = make([]int, 0, 16)
+	inOrder(root)
+
+	for i := len(arr) - 1; i >= 1; i-- {
+		if arr[i] <= arr[i-1] {
+			return false
+		}
+	}
+	return true
+}
+
+func inOrder(node *TreeNode) {
+	if node == nil {
+		return
+	}
+	inOrder(node.Left)
+	arr = append(arr, node.Val)
+	if len(arr) > 1 {
+		if arr[len(arr)-1] <= arr[len(arr)-2] {
+			return
+		}
+	}
+	inOrder(node.Right)
+}
