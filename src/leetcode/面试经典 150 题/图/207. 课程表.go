c12447a43e240c5c8863ed5cacb7795df28f3f8b
renpeng <renpeng@hellogroup.com>
2025-07-15 15:51:46 +0800

    add

--- src/leetcode/面试经典 150 题/图/207. 课程表.go
+++ src/leetcode/面试经典 150 题/图/207. 课程表.go
@@ -14,6 +14,10 @@ func canFinish(numCourses int, prerequisites [][]int) bool {
 	for _, v := range prerequisites {
 		cur := v[0]
 		pre := v[1]
+
+		if cur == pre {
+			return false
+		}
 		allCourse[cur] = struct{}{}
 		allCourse[cur] = struct{}{}
 		if edgsNext[pre] == nil {
@@ -91,8 +95,11 @@ func hasCircle(edgs map[int][]int, node int, exist map[int]bool) bool {
 
 func main() {
 	// prerequisites := [][]int{{0, 1}}
-	prerequisites := [][]int{{1, 4}, {2, 4}, {3, 1}, {3, 2}}
-	numCourses := 5
+	// prerequisites := [][]int{{1, 4}, {2, 4}, {3, 1}, {3, 2}}
+	// numCourses := 5
+
+	prerequisites := [][]int{{0, 10}, {3, 18}, {5, 5}, {6, 11}, {11, 14}, {13, 1}, {15, 1}, {17, 4}}
+	numCourses := 20
 
 	res := canFinish(numCourses, prerequisites)
 	fmt.Println("res: ", res)
