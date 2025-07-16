a373b7bd6fbe93d74403099f4b32b45ee78d8315
renpeng <renpeng@hellogroup.com>
2025-07-16 09:10:47 +0800

    add

--- src/leetcode/面试经典 150 题/图/133. 克隆图.go
+++ src/leetcode/面试经典 150 题/图/133. 克隆图.go
@@ -10,69 +10,39 @@ func cloneGraph(node *Node) *Node {
 		return nil
 	}
 
-	exist := make(map[[2]int]struct{}, 16)
-	new := &Node{}
-	cloneExe(node, new, exist)
-	return new
+	existedNode := make(map[int]*Node, 16)
+	existedEdges := make(map[[2]int]struct{}, 16)
 
+	newNode := &Node{}
+	cloneNode(node, newNode, existedNode, existedEdges)
+	return newNode
 }
 
-func cloneExe(source, new *Node, exist map[[2]int]struct{}) {
-	new.Val = source.Val
-	new.Neighbors = make([]*Node, 0, len(source.Neighbors))
+func cloneNode(source, d *Node, existedNode map[int]*Node, existedEdges map[[2]int]struct{}) {
+	d.Val = source.Val
+	d.Neighbors = make([]*Node, 0, len(source.Neighbors))
+	existedNode[d.Val] = d
 
-	pathExist := 0
-	for _, node := range source.Neighbors {
-		if _, ok := exist[[2]int{source.Val, node.Val}]; ok {
-			pathExist++
+	existedEdgesCnt := 0
+	for _, n := range source.Neighbors {
+		if _, ok := existedEdges[[2]int{source.Val, n.Val}]; ok {
+			existedEdgesCnt++
+			continue
+		}
+
+		if node, ok := existedNode[n.Val]; ok {
+			d.Neighbors = append(d.Neighbors, node)
+			existedEdges[[2]int{source.Val, n.Val}] = struct{}{}
 		} else {
 			newNode := &Node{}
-			cloneExe(node, newNode, exist)
-			new.Neighbors = append(new.Neighbors, newNode)
-			exist[[2]int{source.Val, node.Val}] = struct{}{}
+			cloneNode(n, newNode, existedNode, existedEdges)
+			d.Neighbors = append(d.Neighbors, newNode)
+			existedEdges[[2]int{source.Val, newNode.Val}] = struct{}{}
+			existedNode[newNode.Val] = newNode
 		}
 	}
 
-	if pathExist == len(source.Neighbors) {
+	if existedEdgesCnt == len(source.Neighbors) {
 		return
 	}
 }
-
-// func clone(source, new *Node, exist map[[2]int]struct{}) {
-// 	new.Val = source.Val
-// 	new.Neighbors = make([]*Node, 0, len(source.Neighbors))
-
-// 	pathExist := 0
-// 	for _, n := range source.Neighbors {
-// 		if _,ok := exist[[2]int[source.Val,n.Val]];ok{
-// 			pathExist++
-// 		}else{
-// exist[[2]int[source.Val,n.Val]] = struct{}{}
-// // 		}
-// 	}
-// }
-
-// func main() {
-// 	node1 := &Node{
-// 		Val: 1,
-// 	}
-// 	node2 := &Node{
-// 		Val: 2,
-// 	}
-// 	node3 := &Node{
-// 		Val: 3,
-// 	}
-// 	node4 := &Node{
-// 		Val: 4,
-// 	}
-
-// 	node1.Neighbors = []*Node{node2, node4}
-// 	node2.Neighbors = []*Node{node1, node3}
-// 	node3.Neighbors = []*Node{node2, node4}
-// 	node4.Neighbors = []*Node{node1, node3}
-
-// 	newNode1 := cloneGraph(node1)
-
-// 	fmt.Println
-
-// }
