2efec9462ed834bc556e756200db4a8067143fad
renpeng <renpeng@hellogroup.com>
2025-07-17 10:19:43 +0800

    add

---
+++ src/leetcode/面试经典 150 题/图/210. 课程表 II.go
@@ -0,0 +1,47 @@
+package main
+
+var valid bool
+
+var res []int
+
+func findOrder(numCourses int, prerequisites [][]int) []int {
+	edgeNext := make(map[int][]int, 16)
+	valid = true
+	res = make([]int, 0, 16)
+	for _, q := range prerequisites {
+		cur := q[0]
+		pre := q[1]
+
+		if edgeNext[pre] == nil {
+			edgeNext[pre] = make([]int, 0, 16)
+		}
+		edgeNext[pre] = append(edgeNext[pre], cur)
+	}
+
+	pathSet := make(map[int]int, 16)
+	for v := 0; v < numCourses && valid; v++ {
+		if pathSet[v] == 0 {
+			isCircleByDfs(edgeNext, v, pathSet)
+		}
+	}
+	return res
+}
+
+func isCircleByDfs(edgeNext map[int][]int, curCourse int, pathSet map[int]int) {
+	pathSet[curCourse] = 1
+	for _, v := range edgeNext[curCourse] {
+		if pathSet[v] == 0 {
+
+			if !valid {
+				return
+			}
+			isCircleByDfs(edgeNext, v, pathSet)
+		} else if pathSet[v] == 1 {
+			valid = false
+			return
+		}
+
+	}
+	pathSet[curCourse] = 2 //被2标记过的公共路径不在重复走
+	res = append(res, curCourse)
+}
