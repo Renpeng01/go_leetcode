82662e5c362bf7e451a93a2952d8faeaf2b9cfa8
renpeng <renpeng@hellogroup.com>
2025-07-17 10:08:59 +0800

    add

--- src/leetcode/面试经典 150 题/图/207. 课程表.go
+++ src/leetcode/面试经典 150 题/图/207. 课程表.go
@@ -17,30 +17,31 @@ func canFinish(numCourses int, prerequisites [][]int) bool {
 		edgeNext[pre] = append(edgeNext[pre], cur)
 	}
 
+	pathSet := make(map[int]int, 16)
 	for v := 0; v < numCourses; v++ {
-		if !valid {
-			return false
+		if pathSet[v] == 0 {
+			isCircleByDfs(edgeNext, v, pathSet)
 		}
-		pathSet := make(map[int]bool, 16)
-		isCircleByDfs(edgeNext, v, pathSet)
-
 	}
 	return valid
 }
 
-func isCircleByDfs(edgeNext map[int][]int, curCourse int, pathSet map[int]bool) {
-	if pathSet[curCourse] {
-		valid = false
-		return
-	}
-	pathSet[curCourse] = true
+func isCircleByDfs(edgeNext map[int][]int, curCourse int, pathSet map[int]int) {
+	pathSet[curCourse] = 1
 	for _, v := range edgeNext[curCourse] {
-		if !valid {
+		if pathSet[v] == 0 {
+
+			if !valid {
+				return
+			}
+			isCircleByDfs(edgeNext, v, pathSet)
+		} else if pathSet[v] == 1 {
+			valid = false
 			return
 		}
-		isCircleByDfs(edgeNext, v, pathSet)
-		pathSet[v] = false
+
 	}
+	pathSet[curCourse] = 2
 }
 
 func main() {
