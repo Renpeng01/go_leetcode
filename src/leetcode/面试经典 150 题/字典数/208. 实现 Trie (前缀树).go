8e7ba84cf6e329bdfabb7028be7c974a1a2fd7be
renpeng <renpeng@hellogroup.com>
2025-07-18 15:00:23 +0800

    add

---
+++ src/leetcode/面试经典 150 题/字典数/208. 实现 Trie (前缀树).go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+)
+
+type TrieNoe struct {
+	childern []*TrieNoe
+}
+
+type Trie struct {
+	root *TrieNoe
+}
+
+func Constructor() Trie {
+	tire := Trie{
+		root: &TrieNoe{},
+	}
+	return tire
+}
+
+func (this *Trie) Insert(word string) {
+	node := this.root
+	for i := 0; i < len(word); i++ {
+		if len(node.childern) == 0 {
+			node.childern = make([]*TrieNoe, 26)
+		}
+		node.childern[word[i]-97] = &TrieNoe{}
+		node = node.childern[word[i]-97]
+	}
+}
+
+func (this *Trie) Search(word string) bool {
+	node := this.find(word)
+	return node != nil && len(node.childern) == 0
+
+}
+
+func (this *Trie) find(word string) *TrieNoe {
+	node := this.root
+	for i := 0; i < len(word); i++ {
+		if len(node.childern) == 0 || node.childern[word[i]-97] == nil {
+			return nil
+		}
+		node = node.childern[word[i]-97]
+	}
+	return node
+}
+
+func (this *Trie) StartsWith(prefix string) bool {
+	return this.find(prefix) != nil
+}
+
+func main() {
+	aa := 'a'
+	fmt.Println(aa) // 97
+}
