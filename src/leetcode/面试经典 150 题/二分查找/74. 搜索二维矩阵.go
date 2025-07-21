0136496fece493b7a14856c7a2c10613f965d47c
renpeng <renpeng@hellogroup.com>
2025-07-21 11:47:19 +0800

    add

--- src/leetcode/面试经典 150 题/二分查找/74. 搜索二维矩阵.go
+++ src/leetcode/面试经典 150 题/二分查找/74. 搜索二维矩阵.go
@@ -9,10 +9,10 @@ func searchMatrix(matrix [][]int, target int) bool {
 	var mid int
 	for lo <= hi {
 		mid = lo + (hi-lo)/2
-		// if matrix[mid][0] == target {
-		// 	return true
-		// }
-		if matrix[mid][0] >= target {
+		if matrix[mid][hi] == target {
+			return true
+		}
+		if matrix[mid][0] > target {
 			hi = mid - 1
 		} else {
 			lo = mid + 1
@@ -20,11 +20,15 @@ func searchMatrix(matrix [][]int, target int) bool {
 
 	}
 
+	// if hi < 0 {
+	// 	return false
+	// }
+
+	// fmt.Println("hi: ", hi)
+
 	if matrix[mid][hi] == target {
 		return true
 	}
-
-	// fmt.Printf("lo: %+v, hi: %+v, mid: %+v\n", lo, hi, mid)
 
 	row := hi
 
@@ -46,8 +50,11 @@ func searchMatrix(matrix [][]int, target int) bool {
 }
 
 func main() {
-	matrix := [][]int{{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 50}}
-	target := 11
+	// matrix := [][]int{{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 50}}
+	// target := 11
+
+	matrix := [][]int{{1}}
+	target := 1
 
 	res := searchMatrix(matrix, target)
 	fmt.Println("res:", res)
