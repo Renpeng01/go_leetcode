0037433e3a99d28e7da49e72ebcc242a722cb0db
renpeng <renpeng@hellogroup.com>
2025-07-21 11:25:48 +0800

    add

--- src/leetcode/面试经典 150 题/二分查找/74. 搜索二维矩阵.go
+++ src/leetcode/面试经典 150 题/二分查找/74. 搜索二维矩阵.go
@@ -1,49 +1,38 @@
 package main
 
 func searchMatrix(matrix [][]int, target int) bool {
+	lo := 0
+	hi := len(matrix) - 1
 
-	r := 0
-	c := 0
-	for r < len(matrix) && r > 0 && c < len(matrix[0]) && c > 0 {
-		rLo := 0
-		rHi := len(matrix)
-		rMid := rLo + (rHi-rLo)/2
-		for rLo < rHi {
-			rMid = rLo + (rHi-rLo)/2
-			if matrix[0][rMid] == target {
-				return true
-			}
-			if target > matrix[0][rMid] {
-				rLo = rMid + 1
-			} else {
-				rHi = rMid - 1
-			}
+	var mid int
+	for lo <= hi {
+		mid = lo + (hi-lo)/2
+		if matrix[mid][0] == target {
+			return true
 		}
-		r = rLo
+		if matrix[mid][0] > target {
+			hi = mid - 1
+		} else {
+			lo = mid + 1
+		}
 
-		cLo := 0
-		cHi := len(matrix[0])
-		cMid := cLo + (cHi-cLo)/2
-		for cLo < cHi {
-			cMid = cLo + (cHi-cLo)/2
-			if matrix[cMid][0] == target {
-				return true
-			}
-			if target > matrix[0][cMid] {
-				cLo = cMid + 1
-			} else {
-				cHi = cMid - 1
-			}
-		}
-		c = cLo
 	}
 
+	row := mid
+
+	lo = 0
+	hi = len(matrix[0]) - 1
+
+	for lo <= hi {
+		mid = lo + (hi-lo)/2
+		if matrix[row][mid] == target {
+			return true
+		}
+		if matrix[row][mid] > target {
+			hi = mid - 1
+		} else {
+			lo = mid + 1
+		}
+	}
 	return false
-}
-
-func main() {
-	matrix := [][]int{{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}}
-	target := 3
-	searchMatrix(matrix, target)
-
 }
