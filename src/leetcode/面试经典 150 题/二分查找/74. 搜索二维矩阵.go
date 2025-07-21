41ef5e8d46d38c7e2d4a01d6629f94bde86da925
renpeng <renpeng@hellogroup.com>
2025-07-21 14:57:14 +0800

    add

--- src/leetcode/面试经典 150 题/二分查找/74. 搜索二维矩阵.go
+++ src/leetcode/面试经典 150 题/二分查找/74. 搜索二维矩阵.go
@@ -6,43 +6,17 @@ import (
 
 func searchMatrix(matrix [][]int, target int) bool {
 	lo := 0
-	hi := len(matrix) - 1
-
-	size := len(matrix)
-
-	var mid int
-	for lo <= hi {
-		mid = lo + (hi-lo)/2
-		if matrix[mid][0] == target {
-			return true
-		}
-		if matrix[mid][0] > target {
-			if (mid-1 >= 0 && matrix[mid-1][0] < target) || mid == 0 {
-				// fmt.Println(222)
-				break
-			}
-			hi = mid - 1
-		} else {
-			if (mid+1 <= size-1 && matrix[mid+1][0] > target) || mid == size-1 {
-				// fmt.Println(1111)
-				break
-			}
-			lo = mid + 1
-		}
-	}
-
-	fmt.Println(mid)
-	row := mid
-
-	lo = 0
-	hi = len(matrix[0]) - 1
+	hi := len(matrix)*len(matrix[0]) - 1
 
 	for lo <= hi {
-		mid = lo + (hi-lo)/2
-		if matrix[row][mid] == target {
+		mid := lo + (hi-lo)/2
+		r := mid / len(matrix[0])
+		c := mid % len(matrix[0])
+
+		if matrix[r][c] == target {
 			return true
 		}
-		if matrix[row][mid] > target {
+		if matrix[r][c] > target {
 			hi = mid - 1
 		} else {
 			lo = mid + 1
@@ -52,8 +26,8 @@ func searchMatrix(matrix [][]int, target int) bool {
 }
 
 func main() {
-	matrix := [][]int{{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 50}}
-	target := 30
+	matrix := [][]int{{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}}
+	target := 3
 
 	// matrix := [][]int{{1}}
 	// target := 1
