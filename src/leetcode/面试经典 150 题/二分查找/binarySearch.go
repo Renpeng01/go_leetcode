aa56968d0e94dce41ea81961ce4a31062e173452
renpeng <renpeng@hellogroup.com>
2025-07-21 17:44:45 +0800

    add

--- src/leetcode/面试经典 150 题/二分查找/binarySearch.go
+++ src/leetcode/面试经典 150 题/二分查找/binarySearch.go
@@ -49,6 +49,47 @@ func binarySearch2(nums []int, target int) int {
 	return -1
 }
 
+// 第一个大于等于target的index
+func binarySearch3(nums []int, target int) int {
+	l := 0
+	r := len(nums) - 1
+
+	for l <= r {
+		mid := l + (r-l)/2
+
+		if nums[mid] >= target {
+			if mid == 0 || nums[mid-1] < target {
+				return mid
+			}
+			r = mid - 1
+		} else if nums[mid] < target {
+			l = mid + 1
+		}
+	}
+	return -1
+}
+
+// 最后一个大于等于target
+func binarySearch4(nums []int, target int) int {
+	l := 0
+	r := len(nums) - 1
+
+	for l <= r {
+		mid := l + (r-l)/2
+
+		if nums[mid] <= target {
+			if mid == len(nums)-1 || nums[mid+1] > target {
+				return mid
+			}
+			l = mid + 1
+		} else {
+			r = mid - 1
+
+		}
+	}
+	return -1
+}
+
 func main() {
 	// nums := []int{1, 2, 3, 3, 3, 4}
 	nums := []int{1, 1, 1, 1, 1}
