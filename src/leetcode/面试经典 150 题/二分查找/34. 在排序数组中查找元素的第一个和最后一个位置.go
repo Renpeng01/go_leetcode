a8e82f1f5bb96eecd19297b1d56aee7abffd1c6a
renpeng <renpeng@hellogroup.com>
2025-07-21 17:28:52 +0800

    add

--- src/leetcode/面试经典 150 题/二分查找/34. 在排序数组中查找元素的第一个和最后一个位置.go
+++ src/leetcode/面试经典 150 题/二分查找/34. 在排序数组中查找元素的第一个和最后一个位置.go
@@ -1,9 +1,13 @@
 package main
 
 func searchRange(nums []int, target int) []int {
+	v1 := binarySearch1(nums, target)
+	v2 := binarySearch2(nums, target)
+	return []int{v1, v2}
 
 }
 
+// 第一个等于target的index
 func binarySearch1(nums []int, target int) int {
 	l := 0
 	r := len(nums) - 1
@@ -11,15 +15,20 @@ func binarySearch1(nums []int, target int) int {
 	for l <= r {
 		mid := l + (r-l)/2
 
-		if nums[mid] >= target {
+		if nums[mid] > target {
 			r = mid - 1
-		} else {
+		} else if nums[mid] < target {
 			l = mid + 1
+		} else {
+
+			if mid == 0 || nums[mid-1] != target {
+				return mid
+			} else {
+				r = mid - 1
+			}
 		}
 	}
-
-	return l
-
+	return -1
 }
 
 // 最后一个等于target的index
@@ -30,11 +39,17 @@ func binarySearch2(nums []int, target int) int {
 	for l <= r {
 		mid := l + (r-l)/2
 
-		if nums[mid] <= target {
+		if nums[mid] < target {
 			l = mid + 1
-		} else {
+		} else if nums[mid] > target {
 			r = mid - 1
+		} else {
+			if mid == len(nums)-1 || nums[mid+1] != target {
+				return mid
+			} else {
+				l = mid + 1
+			}
 		}
 	}
-	return l - 1
+	return -1
 }
