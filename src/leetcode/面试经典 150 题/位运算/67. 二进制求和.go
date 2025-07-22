b32576b3fbde5ef44a1f122e760901b48957d306
renpeng <renpeng@hellogroup.com>
2025-07-22 16:19:59 +0800

    add

---
+++ src/leetcode/面试经典 150 题/位运算/67. 二进制求和.go
@@ -0,0 +1,25 @@
+package main
+
+func addBinary(a string, b string) string {
+	carry := '0'
+
+	n := len(a)
+	if len(b) < n {
+		n = len(b)
+	}
+
+	res := ""
+	for i := 0; i < n; i++ {
+		if a[len(a)-1-i] == '1' && b[len(b)-1-i] == '1' {
+			if carry == '0' {
+
+			} else {
+
+			}
+		}else a[len(a)-1-i] == '1' ||b[len(b)-1-i] == '1'{
+
+		}else{
+
+		}
+	}
+}
---
+++ src/leetcode/面试经典 150 题/回溯/17. 电话号码的字母组合.go
@@ -0,0 +1,19 @@
+package main
+
+func letterCombinations(digits string) []string {
+	numLetters := map[byte][]byte{
+		'2': []byte{'a', 'b', 'c'},
+		'3': []byte{'d', 'e', 'f'},
+		'4': []byte{'g', 'h', 'i'},
+		'5': []byte{'j', 'k', 'l'},
+		'6': []byte{'m', 'n', 'o'},
+		'7': []byte{'p', 'q', 'r', 's'},
+		'8': []byte{'t', 'u', 'v'},
+		'9': []byte{'w', 'x', 'y', 'z'},
+	}
+
+	for i := 0; i < len(digits); i++ {
+
+	}
+
+}
