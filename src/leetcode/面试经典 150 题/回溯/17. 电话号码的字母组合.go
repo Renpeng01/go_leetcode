df709de3bb8f6ede7cec429b8bf18b9e4c7da47f
renpeng <renpeng@hellogroup.com>
2025-07-23 16:45:04 +0800

    add

--- src/leetcode/面试经典 150 题/回溯/17. 电话号码的字母组合.go
+++ src/leetcode/面试经典 150 题/回溯/17. 电话号码的字母组合.go
@@ -1,37 +1,35 @@
 package main
 
-import "fmt"
-
 var res []string
 var path []byte
 
 func letterCombinations(digits string) []string {
-	numLetters := map[byte][]byte{
-		'2': []byte{'a', 'b', 'c'},
-		'3': []byte{'d', 'e', 'f'},
-		'4': []byte{'g', 'h', 'i'},
-		'5': []byte{'j', 'k', 'l'},
-		'6': []byte{'m', 'n', 'o'},
-		'7': []byte{'p', 'q', 'r', 's'},
-		'8': []byte{'t', 'u', 'v'},
-		'9': []byte{'w', 'x', 'y', 'z'},
+	if len(digits) == 0 {
+		return []string{}
 	}
-
+	numLetters := [][]byte{
+		[]byte{},
+		[]byte{},
+		[]byte{'a', 'b', 'c'},
+		[]byte{'d', 'e', 'f'},
+		[]byte{'g', 'h', 'i'},
+		[]byte{'j', 'k', 'l'},
+		[]byte{'m', 'n', 'o'},
+		[]byte{'p', 'q', 'r', 's'},
+		[]byte{'t', 'u', 'v'},
+		[]byte{'w', 'x', 'y', 'z'},
+	}
 	res = make([]string, 0, 16)
 	path = make([]byte, 0, 16)
 
-	input := make([][]byte, 0, 16)
-	for i := 0; i < len(digits); i++ {
-		input = append(input, numLetters[digits[i]])
-	}
-	backtracking(input, 0)
-	fmt.Printf("input: %+v\n", input)
+	backtracking(numLetters, digits, 0)
 	return res
 
 }
 
-func backtracking(input [][]byte, k int) {
-	if len(path) == len(input) {
+func backtracking(numLetters [][]byte, digits string, startIndex int) {
+
+	if len(path) == len(digits) {
 		tmp := make([]byte, 0, len(path))
 		for _, c := range path {
 			tmp = append(tmp, c)
@@ -40,18 +38,10 @@ func backtracking(input [][]byte, k int) {
 		return
 	}
 
-	for i := k; i < len(input); i++ {
-		for j := 0; j < len(input[i]); j++ {
-			path = append(path, input[i][j])
-			backtracking(input, k+1)
-			path = path[:len(path)-1]
-		}
+	chars := numLetters[digits[startIndex]-'0']
+	for _, c := range chars {
+		path = append(path, c)
+		backtracking(numLetters, digits, startIndex+1)
+		path = path[:len(path)-1]
 	}
-}
-
-func main() {
-	digits := "23"
-	res := letterCombinations(digits)
-	fmt.Printf("res: %+v\n", res)
-
 }
