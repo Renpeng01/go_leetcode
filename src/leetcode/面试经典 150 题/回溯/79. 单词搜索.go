798545460fc5f0c904ccbeec20d8c18c0abf1224
renpeng <renpeng@hellogroup.com>
2025-07-24 16:47:47 +0800

    add

--- src/leetcode/面试经典 150 题/回溯/79. 单词搜索.go
+++ src/leetcode/面试经典 150 题/回溯/79. 单词搜索.go
@@ -4,33 +4,31 @@ import "fmt"
 
 var res [][][2]int
 var path [][2]int
-
 var pathSet map[[2]int]struct{}
+
+var steps [][]int = [][]int{{1, 1}, {-1, -1}, {1, -1}, {-1, 1}}
 
 func exist(board [][]byte, word string) bool {
 	res = make([][][2]int, 0, 16)
 	path = make([][2]int, 0, 16)
 	pathSet = make(map[[2]int]struct{}, 16)
-	backtracking(board, 0, 0, 0, word)
+	for i := 0; i < len(board); i++ {
+		for j := 0; j < len(board[0]); j++ {
+			backtracking(board, i, j, 0, word)
+		}
+	}
 	fmt.Println(res)
 	return len(res) > 0
 }
 
-func backtracking(board [][]byte, ii, jj, n int, word string) {
-	for i := ii; i < len(board); i++ {
-		for j := jj; j < len(board[0]); j++ {
-			if _, ok := pathSet[[2]int{i, j}]; ok {
-				break
-			}
-
-			if board[i][j] != word[n] {
-				break
-			}
-
+func backtracking(board [][]byte, i, j, n int, word string) {
+	for _, step := range steps {
+		if board[i][j] == word[n] {
 			path = append(path, [2]int{i, j})
 			pathSet[[2]int{i, j}] = struct{}{}
 
-			if n+1 == len(word) {
+			if len(path) == len(word) {
+				fmt.Println(path)
 				tmp := make([][2]int, 0, 16)
 				for _, pair := range path {
 					tmp = append(tmp, pair)
@@ -38,35 +36,12 @@ func backtracking(board [][]byte, ii, jj, n int, word string) {
 				res = append(res, tmp)
 				return
 			}
-			// 上
-			if i-1 >= 0 {
-				if _, ok := pathSet[[2]int{i - 1, j}]; !ok {
-					backtracking(board, i-1, j, n+1, word)
-				}
-			}
 
-			// 下
-			if i+1 < len(board) {
-				if _, ok := pathSet[[2]int{i + 1, j}]; !ok {
-					backtracking(board, i+1, j, n+1, word)
-				}
+			if _, ok := pathSet[[2]int{i + step[0], j + step[1]}]; !ok && i+step[0] >= 0 && i+step[0] < len(board) && j+step[1] > 0 && j+step[1] < len(board[0]) {
+				backtracking(board, i+step[0], j+step[1], n+1, word)
 			}
-			// 左
-			if j-1 >= 0 {
-				if _, ok := pathSet[[2]int{i, j - 1}]; !ok {
-					backtracking(board, i, j-1, n+1, word)
-				}
-			}
-			// 右
-			if j+1 < len(board[0]) {
-				if _, ok := pathSet[[2]int{i, j + 1}]; !ok {
-					backtracking(board, i, j+1, n+1, word)
-				}
-			}
-
 			delete(pathSet, path[len(path)-1])
 			path = path[:len(path)-1]
-
 		}
 	}
 }
