867491d833cd9a518a26367f6880561beefa338e
renpeng <renpeng@hellogroup.com>
2025-07-24 15:05:36 +0800

    add

--- src/leetcode/面试经典 150 题/回溯/22. 括号生成.go
+++ src/leetcode/面试经典 150 题/回溯/22. 括号生成.go
@@ -2,10 +2,12 @@ package main
 
 var res []string
 var path []byte
+var done map[string]struct{}
 
 func generateParenthesis(n int) []string {
 	res = make([]string, 0, 16)
 	path = make([]byte, 0, 16)
+	done := make(map[string]struct{}, 16)
 	backtracking(0, n)
 	return res
 }
@@ -15,4 +17,16 @@ func backtracking(cur, n int) {
 		return
 	}
 
+	tmp := make([]byte, 0, len(path))
+	for _, v := range path {
+		tmp = append(tmp, v)
+	}
+
+	for i, c := range tmp {
+
+		if _, ok := done[string(path)]; ok {
+			continue
+		}
+		backtracking(cur+1, n)
+	}
 }
