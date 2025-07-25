bbf1c1056919c854a37c6db1cb729e630133840c
renpeng <renpeng@hellogroup.com>
2025-07-25 15:44:32 +0800

    add

--- src/leetcode/面试经典 150 题/位运算/190. 颠倒二进制位.go
+++ src/leetcode/面试经典 150 题/位运算/190. 颠倒二进制位.go
@@ -15,6 +15,12 @@ func reverseBits(n int) int {
 		bits = append(bits, a)
 	}
 
+	bitsLen := len(bits)
+	for i := 0; i < 32-bitsLen; i++ {
+		bits = append(bits, 0)
+	}
+	// fmt.Println(bits)
+
 	res := 0
 	for i := len(bits) - 1; i >= 0; i-- {
 		if bits[len(bits)-1-i] != 0 {
@@ -25,6 +31,6 @@ func reverseBits(n int) int {
 }
 
 func main() {
-	res := reverseBits(11)
+	res := reverseBits(43261596)
 	fmt.Println(res)
 }
