92263f3fc9b818c30a8ee4b31457914bf99c316b
renpeng <renpeng@hellogroup.com>
2025-07-25 14:38:01 +0800

    add

---
+++ src/leetcode/面试经典 150 题/位运算/190. 颠倒二进制位.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func reverseBits(n int) int {
+
+	bits := make([]int, 0, 16)
+
+	for n > 0 {
+		a := n % 2
+		n = n / 2
+		bits = append(bits, a)
+	}
+
+	res := 0
+	for i := len(bits) - 1; i >= 0; i-- {
+		if bits[len(bits)-1-i] != 0 {
+			res += int(math.Pow(2, float64(i)))
+		}
+	}
+	return res
+}
+
+func main() {
+	res := reverseBits(43261596)
+	fmt.Println(res)
+}
