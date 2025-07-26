f7841941c4a1aeca0aaeb85ba91c0d5773d96ee1
renpeng <renpeng@p1.com>
2025-07-26 21:39:24 +0800

    add

---
+++ src/代码随想录/动态规划/343. 整数拆分***.go
@@ -0,0 +1,32 @@
+package main
+
+// 本题难点在于想不到用动态规划的方法解决
+func integerBreak(n int) int {
+	if n == 0 {
+		return 0
+	}
+
+	if n == 1 || n == 2 {
+		return 1
+	}
+
+	dp := make([]int, n+1)
+	dp[0] = 0
+	dp[1] = 1
+	dp[2] = 1
+
+	for i := 3; i <= n; i++ {
+		for j := 1; j < i; j++ {
+			dp[i] = max(dp[i])
+		}
+	}
+
+	return dp[n]
+
+}
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
