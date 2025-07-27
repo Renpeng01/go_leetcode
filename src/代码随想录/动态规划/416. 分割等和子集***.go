1f162a03394cfa7dbfffcbcd1b5aa466581ad181
renpeng <renpeng@p1.com>
2025-07-27 15:07:40 +0800

    add

--- src/代码随想录/动态规划/416. 分割等和子集***.go
+++ src/代码随想录/动态规划/416. 分割等和子集***.go
@@ -1,7 +1,7 @@
 package main
 
 // 可以转化为01背包问题，物品是否可以把容量为sum一半的背包装满
-func canPartition(nums []int) bool {
+func canPartition1(nums []int) bool { // 二维dp数组
 
 	sum := 0
 	for _, v := range nums {
@@ -42,4 +42,33 @@ func max(a, b int) int {
 		return a
 	}
 	return b
+}
+
+func canPartition(nums []int) bool { // 一维dp数组
+
+	sum := 0
+	for _, v := range nums {
+		sum += v
+	}
+
+	if sum%2 != 0 {
+		return false
+	}
+
+	bag := sum / 2
+
+	dp := make([]int, bag+1)
+
+	for j := nums[0]; j <= bag; j++ {
+		dp[j] = nums[0]
+	}
+
+	for i := 1; i < len(nums); i++ {
+		for j := bag; j >= nums[i]; j-- {
+			dp[j] = max(dp[j], dp[j-nums[i]]+nums[i])
+		}
+
+	}
+
+	return dp[bag] == bag
 }
