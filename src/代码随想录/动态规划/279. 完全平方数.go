4a2f43d18ea40a0a1039fa9d078ac9871050c7e4
renpeng <renpeng@p1.com>
2025-07-29 21:39:31 +0800

    add

---
+++ src/代码随想录/动态规划/279. 完全平方数.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func numSquares(n int) int {
+
+	dp := make([]int, n+1)
+	for i := 0; i < len(dp); i++ {
+		dp[i] = math.MaxInt
+	}
+	dp[0] = 0
+	for i := 1; i <= n/2+1; i++ {
+		for j := i * i; j <= n; j++ {
+			if dp[j-i*i] != math.MinInt {
+				dp[j] = min(dp[j], dp[j-i*i]+1)
+			}
+		}
+
+	}
+	return dp[n]
+
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	res := numSquares(12)
+	fmt.Println(res)
+}
