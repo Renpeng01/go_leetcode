d4498e69f1ccd25af972f34a0c6a46d8460316af
renpeng <renpeng@hellogroup.com>
2025-07-29 14:47:58 +0800

    add

--- src/代码随想录/动态规划/377. 组合总和 Ⅳ.go
+++ src/代码随想录/动态规划/377. 组合总和 Ⅳ.go
@@ -2,4 +2,16 @@ package main
 
 func combinationSum4(nums []int, target int) int {
 
+	dp := make([]int, target+1)
+	dp[0] = 1
+
+	for j := 1; j <= target; j++ {
+		for i := 0; i < len(nums); i++ {
+			if j >= nums[i] {
+				dp[j] += dp[j-nums[i]]
+			}
+
+		}
+	}
+	return dp[target]
 }
