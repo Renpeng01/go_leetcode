58e957b12760c73470033bb8092bcb2d7fcb115d
renpeng <renpeng@hellogroup.com>
2025-07-30 10:11:20 +0800

    add

---
+++ src/leetcode/面试经典 150 题/多维动态规划/221. 最大正方形.go
@@ -0,0 +1,34 @@
+package main
+
+func maximalSquare(matrix [][]byte) int {
+	dp := make([][]int, len(matrix))
+
+	for i := 0; i < len(matrix); i++ {
+		dp[i] = make([]int, len(matrix[i]))
+	}
+
+	dp[0][0] = 0
+	if matrix[0][0] == '1' {
+		dp[0][0] = 1
+	}
+
+	for i := 1; i < len(matrix[0]); i++ {
+		if matrix[0][i] == '1' {
+			dp[0][i] = dp[0][i-1] + 1
+		}
+	}
+
+	for i := 1; i < len(matrix); i++ {
+		if matrix[i][0] == '1' {
+			dp[i][0] = dp[i-1][0] + 1
+		}
+	}
+
+	for i := 1; i < len(matrix); i++ {
+		for j := 1; j < len(matrix[0]); j++ {
+			if matrix[i][j] == '1' {
+
+			}
+		}
+	}
+}
