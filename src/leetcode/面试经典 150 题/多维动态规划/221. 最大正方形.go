b457cf7b2a530cc334886f925c9d945c13ab3e5f
renpeng <renpeng@hellogroup.com>
2025-07-30 15:39:35 +0800

    add

--- src/leetcode/面试经典 150 题/多维动态规划/221. 最大正方形.go
+++ src/leetcode/面试经典 150 题/多维动态规划/221. 最大正方形.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 func maximalSquare(matrix [][]byte) int {
 	dp := make([][]int, len(matrix))
 
@@ -24,24 +28,47 @@ func maximalSquare(matrix [][]byte) int {
 		}
 	}
 
+	// for _, v := range dp {
+	// 	fmt.Println(v)
+	// }
 	for i := 1; i < len(matrix); i++ {
-		for j := 1; j < len(matrix[0]); j++ {
+		for j := 1; j < len(matrix[i]); j++ {
+
 			if matrix[i][j] == '1' {
-				// dp[i][j] = dp[i-1][j]
-
-				if 
-			} else {
-
+				dp[i][j] = min(dp[i-1][j], min(dp[i][j-1], dp[i-1][j-1])) + 1
 			}
+
 		}
 	}
 
-	return dp[len(matrix)-1][len(matrix[0])-1]
+	// for _, v := range dp {
+	// 	fmt.Println(v)
+	// }
+
+	maxEdge := 0
+	for i := 0; i < len(dp); i++ {
+		for j := 0; j < len(dp[i]); j++ {
+			if dp[i][j] > maxEdge {
+				maxEdge = dp[i][j]
+			}
+		}
+	}
+	return maxEdge * maxEdge
 }
 
-func max(a,b int) int{
-	if a > b{
+func min(a, b int) int {
+	if a < b {
 		return a
 	}
 	return b
+}
+
+func main() {
+	matrix := [][]byte{
+		{'1', '0', '1', '0', '0'},
+		{'1', '0', '1', '1', '1'},
+		{'1', '1', '1', '1', '1'},
+		{'1', '0', '0', '1', '0'}}
+	res := maximalSquare(matrix)
+	fmt.Println(res)
 }
