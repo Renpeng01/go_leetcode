6119d1e463785f1526b4b44aa77459e99786f616
renpeng <renpeng@hellogroup.com>
2025-08-01 17:00:44 +0800

    add

--- src/leetcode/面试经典 150 题/数学/50. Pow(x, n)***.go
+++ src/leetcode/面试经典 150 题/数学/50. Pow(x, n)***.go
@@ -2,7 +2,7 @@ package main
 
 import "math"
 
-// https://www.bilibili.com/video/BV1P3411j72h/?spm_id_from=333.337.search-card.all.click&vd_source=70c464e99440c207e9933663bb2e5166
+// https://www.bilibili.com/video/BV19c411H7Pq/?spm_id_from=333.337.search-card.all.click&vd_source=70c464e99440c207e9933663bb2e5166
 func myPow(x float64, n int) float64 {
 	if n == 0 {
 		return 1
