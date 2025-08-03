03ab9408d505f4199f4e1215e9d3372392254342
renpeng <renpeng@p1.com>
2025-08-03 09:45:20 +0800

    add

--- src/代码随想录/动态规划/647. 回文子串.go
+++ src/代码随想录/动态规划/647. 回文子串.go
@@ -1,6 +1,7 @@
 package main
 
-func countSubstrings(s string) int {
+// 相当于暴力解法
+func countSubstrings1(s string) int {
 
 	dp := make([]int, len(s))
 
@@ -11,9 +12,11 @@ func countSubstrings(s string) int {
 	sum := dp[0]
 	for i := 1; i < len(s); i++ {
 		for j := 0; j < i; j++ {
-			if isPalindrome(s[j : i+1]) {
-				dp[i]++
-			}
+			// 没有递推关系，属于暴力算法 *******
+			// if isPalindrome(s[j : i+1]) {
+			// 	dp[i]++ //
+			// }
+
 		}
 		sum += dp[i]
 	}
@@ -37,4 +40,29 @@ func isPalindrome(s string) bool {
 		r--
 	}
 	return true
+}
+
+func countSubstrings(s string) int {
+	// dp[i][j] [i,j]之间的子串是否是回文
+	dp := make([][]bool, len(s))
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]bool, len(s))
+	}
+
+	res := 0
+	for i := len(s) - 1; i >= 0; i-- {
+		for j := 0; j <= i; j++ {
+			if s[i] == s[j] {
+				if j-i <= 1 {
+					dp[i][j] = true
+					res++
+				} else if dp[i+1][j-1] {
+					dp[i][j] = true
+					res++
+				}
+			}
+		}
+	}
+	return res
+
 }
