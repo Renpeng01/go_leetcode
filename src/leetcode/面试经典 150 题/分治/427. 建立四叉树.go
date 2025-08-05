fd2e85c269183609ec5c1f1b0998bd20fe970c77
renpeng <renpeng@hellogroup.com>
2025-08-05 17:35:02 +0800

    add

--- src/leetcode/面试经典 150 题/分治/427. 建立四叉树.go
+++ src/leetcode/面试经典 150 题/分治/427. 建立四叉树.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 type Node struct {
 	Val         bool
 	IsLeaf      bool
@@ -12,72 +10,26 @@ type Node struct {
 }
 
 func construct(grid [][]int) *Node {
-	if len(grid) == 1 {
+	return build(grid, 0, len(grid)-1, 0, len(grid[0])-1)
+}
+
+func build(grid [][]int, left, right, top, bottom int) *Node {
+	if left == right && top == bottom {
 		node := &Node{}
-		if grid[0][0] == 1 {
+		if grid[left][top] == 1 {
 			node.Val = true
 			node.IsLeaf = true
 		}
 		return node
 	}
 
-	topLeftGrid := make([][]int, 0, 256)
-	topRightGrid := make([][]int, 0, 256)
-	bottomLeftGrid := make([][]int, 0, 256)
-	bottomRightGrid := make([][]int, 0, 256)
-
-	for i := 0; i < len(grid); i++ {
-		topLeftItem := make([]int, 0, 4)
-		topRightItem := make([]int, 0, 4)
-		bottomLeftItem := make([]int, 0, 4)
-		bottomRightItem := make([]int, 0, 4)
-
-		for j := 0; j < len(grid[i]); j++ {
-			if i >= 0 && i <= len(grid)/2 {
-				if j >= 0 && j <= len(grid[i])/2 {
-					topLeftItem = append(topLeftItem, grid[i][j])
-				} else {
-					topRightItem = append(topRightItem, grid[i][j])
-				}
-			} else {
-				if j >= 0 && j <= len(grid[i])/2 {
-					bottomLeftItem = append(bottomLeftItem, grid[i][j])
-				} else {
-					bottomRightItem = append(bottomRightItem, grid[i][j])
-				}
-			}
-
-			if len(topLeftItem) > 0 {
-				topLeftGrid = append(topLeftGrid, topLeftItem)
-			}
-			if len(bottomLeftItem) > 0 {
-				bottomLeftGrid = append(bottomLeftGrid, bottomLeftItem)
-
-			}
-			if len(topRightItem) > 0 {
-				topRightGrid = append(topRightGrid, topRightItem)
-
-			}
-			if len(bottomRightItem) > 0 {
-				bottomRightGrid = append(bottomRightGrid, bottomRightItem)
-			}
-		}
-	}
-
-	fmt.Println(topLeftGrid)
-	fmt.Println(topRightGrid)
-	fmt.Println(bottomLeftGrid)
-	fmt.Println(bottomRightGrid)
+	horizontalMid := left + (right-left)/2
+	verticalMid := top + (bottom-top)/2
 
 	root := &Node{}
-	// root.TopLeft = construct(topLeftGrid)
-	// root.TopRight = construct(topRightGrid)
-	// root.BottomLeft = construct(bottomLeftGrid)
-	// root.BottomRight = construct(bottomRightGrid)
+	root.TopLeft = build(grid, left, horizontalMid, top, verticalMid)
+	root.TopRight = build(grid, horizontalMid+1, right, top, verticalMid)
+	root.BottomLeft = build(grid, left, horizontalMid, verticalMid+1, bottom)
+	root.BottomRight = build(grid, horizontalMid+1, right, verticalMid+1, bottom)
 	return root
-}
-
-func main() {
-	grid := [][]int{{0, 1}, {1, 0}}
-	construct(grid)
 }
