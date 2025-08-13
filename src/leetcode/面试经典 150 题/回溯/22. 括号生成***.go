77cde52eb80ea384b35bc23ce8b418b97d49bbd5
renpeng <renpeng@hellogroup.com>
2025-08-13 09:23:01 +0800

    add

--- src/leetcode/面试经典 150 题/回溯/22. 括号生成***.go
+++ src/leetcode/面试经典 150 题/回溯/22. 括号生成***.go
@@ -4,26 +4,23 @@ import "fmt"
 
 // https://www.bilibili.com/video/BV14pJLzHEEE/?spm_id_from=333.337.search-card.all.click&vd_source=70c464e99440c207e9933663bb2e5166
 // https://www.bilibili.com/video/BV1xV411q7wE/?spm_id_from=333.337.search-card.all.click&vd_source=70c464e99440c207e9933663bb2e5166
-var exist map[string]struct{}
 var res []string
 
 func generateParenthesis(n int) []string {
-	exist = make(map[string]struct{}, 256)
 	res = make([]string, 0, 256)
 	backtracking("(", 1, 0, n)
 	return res
 }
 
 func backtracking(s string, l, r, n int) {
-	if r > l || r+l > n {
+	if r > l || r+l > 2*n {
 		return
 	}
 
+	// fmt.Println(l, r, n)
 	if l == n && r == n {
-		if _, ok := exist[s]; !ok {
-			res = append(res, s)
-			exist[s] = struct{}{}
-		}
+		res = append(res, s)
+		// fmt.Println(1111)
 		return
 	}
 
