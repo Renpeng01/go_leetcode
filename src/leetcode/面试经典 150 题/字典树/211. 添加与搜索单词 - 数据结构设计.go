266c3526cd8f80bdeb2df4c34507a5b494c40308
renpeng <renpeng@hellogroup.com>
2025-08-14 17:15:48 +0800

    add

--- src/leetcode/面试经典 150 题/字典树/211. 添加与搜索单词 - 数据结构设计.go
+++ src/leetcode/面试经典 150 题/字典树/211. 添加与搜索单词 - 数据结构设计.go
@@ -1,18 +1,51 @@
 package main
 
+type letterNode struct {
+	c        byte
+	isLeaf   bool
+	children [26]*letterNode
+}
+
 type WordDictionary struct {
+	root [26]*letterNode
 }
 
 func Constructor() WordDictionary {
-
+	return WordDictionary{
+		root: [26]*letterNode{},
+	}
 }
 
 func (this *WordDictionary) AddWord(word string) {
+
+	node := this.root
+	for i := 0; i < len(word); i++ {
+		node[word[i]-'a'] = &letterNode{
+			c:        word[i],
+			isLeaf:   i == len(word)-1,
+			children: [26]*letterNode{},
+		}
+		node = node[word[i]-'a'].children
+	}
 
 }
 
 func (this *WordDictionary) Search(word string) bool {
 
+	node := this.root
+	for i := 0; i < len(word); i++ {
+		n := node[int(word[i]-'a')]
+		if n == nil {
+			return false
+		}
+
+		if i == len(word)-1 && n.isLeaf {
+			return true
+		}
+
+		node = n.children
+	}
+	return false
 }
 
 /**
