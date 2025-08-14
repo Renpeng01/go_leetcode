6a9ba0c0dc6c8307272d715002822ae4b61b694b
renpeng <renpeng@hellogroup.com>
2025-08-14 16:58:47 +0800

    add

--- src/leetcode/面试经典 150 题/栈/224. 基本计算器***.go
+++ src/leetcode/面试经典 150 题/栈/224. 基本计算器***.go
@@ -225,3 +225,35 @@ func calculate(s string) int {
 func isNum(c byte) bool {
 	return c == '0' || c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8' || c == '9'
 }
+
+// leetcode 题解
+func calculate2(s string) (ans int) {
+	ops := []int{1}
+	sign := 1
+	n := len(s)
+	for i := 0; i < n; {
+		switch s[i] {
+		case ' ':
+			i++
+		case '+':
+			sign = ops[len(ops)-1]
+			i++
+		case '-':
+			sign = -ops[len(ops)-1]
+			i++
+		case '(':
+			ops = append(ops, sign)
+			i++
+		case ')':
+			ops = ops[:len(ops)-1]
+			i++
+		default:
+			num := 0
+			for ; i < n && '0' <= s[i] && s[i] <= '9'; i++ {
+				num = num*10 + int(s[i]-'0')
+			}
+			ans += sign * num
+		}
+	}
+	return
+}
