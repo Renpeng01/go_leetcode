988c281085c0850ffa1da2989bccf35ae80263d7
renpeng <renpeng@hellogroup.com>
2025-08-14 10:49:55 +0800

    add

--- src/leetcode/面试经典 150 题/栈/224. 基本计算器.go
+++ src/leetcode/面试经典 150 题/栈/224. 基本计算器.go
@@ -1,18 +1,48 @@
 package main
 
+import (
+	"strconv"
+	"strings"
+)
+
 func calculate(s string) int {
+	ops := make([]byte, 0, 16)
+	nums := make([]int, 0, 16)
+
+	for i := 0; i < len(s); i++ {
+		if s[i] == '-' || s[i] == '+' {
+			ops = append(ops, s[i])
+		} else if s[i] == '(' || s[i] == ')' || strings.TrimSpace(string(s[i])) == "" {
+			continue
+		} else {
+			n, _ := strconv.Atoi(string(s[i]))
+			if len(ops) == 0 {
+				nums = append(nums, n)
+			} else {
+				if len(nums) == 0 {
+
+				} else {
+					n1 := nums[len(nums)-1]
+					op := ops[len(ops)-1]
+
+					nums = nums[:len(nums)-1]
+					ops = ops[:len(ops)-1]
+					nums = append(nums, calculateItem(n1, n, string(op)))
+				}
+			}
+		}
+	}
 
 }
 
-// 示例 1：
+func calculateItem(n1, n2 int, op string) int {
+	res := 0
+	switch op {
+	case "-":
+		res = n1 - n2
+	case "+":
+		res = n1 + n2
+	}
+	return res
 
-// 输入：s = "1 + 1"
-// 输出：2
-// 示例 2：
-
-// 输入：s = " 2-1 + 2 "
-// 输出：3
-// 示例 3：
-
-// 输入：s = "(1+(4+5+2)-3)+(6+8)"
-// 输出：23
+}
