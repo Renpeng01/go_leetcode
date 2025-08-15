53c96f999384efbb07ffe7dcf9bc1c21a5d4832b
renpeng <renpeng@hellogroup.com>
2025-08-15 16:47:20 +0800

    add

--- src/leetcode/面试经典 150 题/字典树/212. 单词搜索 II.go
+++ src/leetcode/面试经典 150 题/字典树/212. 单词搜索 II.go
@@ -4,7 +4,7 @@ import "fmt"
 
 var res []string
 var existed map[string]bool
-var visited map[[2]int]bool
+var visited [][]bool
 
 func findWords(board [][]byte, words []string) []string {
 	res = make([]string, 0, len(words))
@@ -13,7 +13,10 @@ func findWords(board [][]byte, words []string) []string {
 	for _, word := range words {
 		for i := 0; i < len(board); i++ {
 			for j := 0; j < len(board[i]); j++ {
-				visited = make(map[[2]int]bool, 256)
+				visited = make([][]bool, len(board))
+				for i := 0; i < len(visited); i++ {
+					visited[i] = make([]bool, len(board[i]))
+				}
 				dfs(board, word, i, j, 0)
 			}
 		}
@@ -22,14 +25,14 @@ func findWords(board [][]byte, words []string) []string {
 }
 
 func dfs(board [][]byte, word string, i, j, wordIndex int) {
-	if visited[[2]int{i, j}] {
+	if visited[i][j] {
 		return
 	}
 
 	if word[wordIndex] != board[i][j] {
 		return
 	}
-	visited[[2]int{i, j}] = true
+	visited[i][j] = true
 
 	if wordIndex == len(word)-1 {
 		if !existed[word] {
@@ -42,25 +45,22 @@ func dfs(board [][]byte, word string, i, j, wordIndex int) {
 	// 上
 	if i > 0 {
 		dfs(board, word, i-1, j, wordIndex+1)
-		visited[[2]int{i - 1, j}] = false
 	}
 
 	// 下
 	if i < len(board)-1 {
 		dfs(board, word, i+1, j, wordIndex+1)
-		visited[[2]int{i + 1, j}] = false
 	}
 
 	// 左
 	if j > 0 {
 		dfs(board, word, i, j-1, wordIndex+1)
-		visited[[2]int{i, j - 1}] = false
 	}
 	// 右
 	if j < len(board[0])-1 {
 		dfs(board, word, i, j+1, wordIndex+1)
-		visited[[2]int{i, j + 1}] = false
 	}
+	visited[i][j] = false
 
 }
 
