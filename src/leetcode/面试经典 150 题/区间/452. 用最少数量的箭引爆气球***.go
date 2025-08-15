e4fe0da3032cabedf39e81a0f7e5466275daa87a
renpeng <renpeng@hellogroup.com>
2025-08-15 15:01:45 +0800

    add

---
+++ src/leetcode/面试经典 150 题/区间/452. 用最少数量的箭引爆气球***.go
@@ -0,0 +1,30 @@
+package main
+
+import "sort"
+
+// https://www.bilibili.com/video/BV1SA41167xe/?spm_id_from=333.337.search-card.all.click&vd_source=70c464e99440c207e9933663bb2e5166
+func findMinArrowShots(points [][]int) int {
+	sort.SliceStable(points, func(i, j int) bool {
+		return points[i][0] < points[j][0]
+	})
+
+	res := 0
+	for i := 1; i < len(points); i++ {
+		if points[i][0] > points[i-1][1] {
+			res++
+		} else {
+			points[i][1] = min(points[i][1], points[i-1][1])
+		}
+
+	}
+	return res + 1
+
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+
+	return b
+}
