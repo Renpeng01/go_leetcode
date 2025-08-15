74a1bc5450daa318f3cf3c2fe6239693dd7542b7
renpeng <renpeng@hellogroup.com>
2025-08-15 15:30:18 +0800

    add

--- src/leetcode/面试经典 150 题/链表/25. K 个一组翻转链表.go
+++ src/leetcode/面试经典 150 题/链表/25. K 个一组翻转链表.go
@@ -7,4 +7,37 @@ type ListNode struct {
 
 func reverseKGroup(head *ListNode, k int) *ListNode {
 
+	dummy := &ListNode{
+		Next: head,
+	}
+	groupPre := dummy
+
+	for {
+		cur := groupPre
+		for k > 0 && cur != nil {
+			cur = cur.Next
+			k--
+		}
+		if cur == nil {
+			break
+		}
+		groupNext := cur.Next
+
+		pre := groupNext
+		cur = groupPre.Next
+		// var tmp *ListNode
+
+		for cur != groupNext {
+			tmp := cur.Next
+			cur.Next = pre
+			pre = cur
+			cur = tmp
+		}
+
+		tmp := groupPre.Next
+		groupPre.Next = pre
+		groupPre = tmp
+	}
+
+	return dummy.Next
 }
