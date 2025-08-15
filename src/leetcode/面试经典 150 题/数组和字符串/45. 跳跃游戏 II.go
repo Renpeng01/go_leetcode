d3e92d19360099d62caa136bb3c9089babe6db00
renpeng <renpeng@hellogroup.com>
2025-08-15 14:26:42 +0800

    add

--- src/leetcode/面试经典 150 题/数组和字符串/45. 跳跃游戏 II.go
+++ src/leetcode/面试经典 150 题/数组和字符串/45. 跳跃游戏 II.go
@@ -1,95 +1,29 @@
 package main
 
-import (
-	"fmt"
-)
+import "math"
 
 func jump(nums []int) int {
-	if len(nums) == 1 {
-		return 1
-	}
-	dp := make([][]int, len(nums))
-	steps := make([][]int, len(nums))
 
-	for i := 0; i < len(nums); i++ {
-		dp[i] = make([]int, 2)
-	}
+	dp := make([]int, len(nums))
 
-	for i := 0; i < len(nums); i++ {
-		steps[i] = []int{100, 100}
+	dp[0] = 0
 
-	}
-
-	dp[0][0] = 0
-	dp[0][1] = nums[0]
-
-	steps[0][1] = 0
-
-	for i := 1; i < len(nums); i++ {
-
-		if dp[i-1][0] >= len(nums)-1 && dp[i-1][1] >= len(nums)-1 {
-			fmt.Println(nums)
-			fmt.Println(dp)
-			fmt.Println(steps)
-			return min(steps[i-1][0], steps[i-1][1])
-		}
-
-		if dp[i-1][0] >= len(nums)-1 {
-			fmt.Println(nums)
-			fmt.Println(dp)
-			fmt.Println(steps)
-			return steps[i-1][0]
-		}
-
-		if dp[i-1][1] >= len(nums)-1 {
-			fmt.Println(nums)
-			fmt.Println(dp)
-			fmt.Println(steps)
-			return steps[i-1][1]
-		}
-		if max(dp[i-1][0], dp[i-1][1]) >= i {
-			dp[i][0] = max(dp[i-1][0], dp[i-1][1])
-			dp[i][1] = i + nums[i]
-		}
-
-		if dp[i-1][0] >= i && dp[i-1][1] >= i {
-			steps[i][0] = min(steps[i-1][0], steps[i-1][1])
-			steps[i][1] = steps[i][0] + 1
-		} else if dp[i-1][0] >= i {
-			steps[i][0] = steps[i-1][0]
-			steps[i][1] = steps[i][0] + 1
-		} else if dp[i-1][1] >= i {
-			steps[i][0] = steps[i-1][1]
-			steps[i][1] = steps[i][0] + 1
+	for i := 1; i < len(dp); i++ {
+		dp[i] = math.MaxInt
+		for j := 0; j < i; j++ {
+			if j+nums[j] >= i {
+				dp[i] = min(dp[i], dp[j]+1)
+			}
 		}
 	}
 
-	// minStep := math.MaxInt
-
-	// for i := 0; i < len(steps); i++ {
-	// 	minStep = min(steps[i][0], steps[i][1])
-	// }
-	// return minStep
-	return -1
-}
-
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
+	return dp[len(dp)-1]
 }
 
 func min(a, b int) int {
-	if a > b {
-		return b
+	if a < b {
+		return a
 	}
-	return a
-}
 
-func main() {
-	nums := []int{3, 4, 3, 2, 5, 4, 3}
-
-	res := jump(nums)
-	fmt.Println(res)
+	return b
 }
