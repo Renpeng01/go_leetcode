1c4f7feee64a491cd81d3bee672d2239e6f2a5d1
renpeng <renpeng@hellogroup.com>
2025-08-29 09:43:38 +0800

    add

--- src/代码随想录/数组/二分查找/704. 二分查找.go
+++ src/代码随想录/数组/二分查找/704. 二分查找.go
@@ -2,9 +2,10 @@ package main
 
 func search(nums []int, target int) int {
 	left, right := 0, len(nums)-1
+	var mid int
 	for left <= right { //  这里是小于还是小于等于，需要模拟总结下(区间（左闭右闭 左闭右开）定义岁影响因素 )
-		mid := left + (right-left)/2
-		if nums[mid] == target {
+		mid = left + (right-left)>>1
+		if nums[left+(right-left)>>1] == target {
 			return mid
 		} else if nums[mid] > target {
 			right = mid - 1
