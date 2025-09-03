7897e34c22d16f6654cd663014cbbbaf4a112a40
renpeng <renpeng@hellogroup.com>
2025-09-03 15:01:41 +0800

    add

---
+++ src/代码随想录/链表/206. 反转链表.go
@@ -0,0 +1,24 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	var pre *ListNode
+	cur := head
+
+	for cur != nil {
+		next := cur.Next
+		cur.Next = pre
+		pre = cur
+		cur = next
+	}
+
+	return pre
+}
