f69f008d10ce5f63a4efa181b61ab20853f88b8d
renpeng <renpeng@hellogroup.com>
2025-09-05 14:47:55 +0800

    add

--- src/代码随想录/哈希表/15. 三数之和.go
+++ src/代码随想录/哈希表/15. 三数之和.go
@@ -35,7 +35,7 @@ func threeSum(nums []int) [][]int {
 	res := make([][]int, 0, 8)
 	for i := 0; i < len(nums)-2; i++ {
 		if nums[i] > 0 {
-			return nil
+			return res
 		}
 
 		if i > 0 && nums[i] == nums[i-1] {
