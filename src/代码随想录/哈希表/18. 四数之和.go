830bc5e22a73b64e9928893536ceb21d0004ab47
renpeng <renpeng@hellogroup.com>
2025-09-05 15:06:43 +0800

    add

--- src/代码随想录/哈希表/18. 四数之和.go
+++ src/代码随想录/哈希表/18. 四数之和.go
@@ -1,5 +1,51 @@
 package main
 
+import "sort"
+
 func fourSum(nums []int, target int) [][]int {
+	sort.Ints(nums)
+	res := make([][]int, 0, 8)
+
+	for j := 0; j < len(nums); j++ {
+		if nums[j] > target {
+			return res
+		}
+
+		if j > 0 && nums[j] == nums[j-1] {
+			continue
+		}
+
+		for i := j + 1; i < len(nums); i++ {
+			if nums[i] > target {
+				return res // 注意这里不能return nil
+			}
+
+			if i > 0 && nums[i] == nums[i-1] {
+				continue
+			}
+			left := i + 1
+			right := len(nums) - 1
+			for left < right {
+				if nums[j]+nums[i]+nums[left]+nums[right] > target {
+					right--
+				} else if nums[j]+nums[i]+nums[left]+nums[right] < target {
+					left++
+				} else {
+					res = append(res, []int{nums[j], nums[i], nums[left], nums[right]})
+					for left < right && nums[right] == nums[right-1] {
+						right--
+					}
+					for left < right && nums[left] == nums[left+1] {
+						left++
+					}
+					left++
+					right--
+				}
+			}
+		}
+
+	}
+
+	return res
 
 }
