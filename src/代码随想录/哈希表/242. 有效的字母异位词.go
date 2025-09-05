bc7bac62e85c8ed2d3d409ed83605ee1086c3e59
renpeng <renpeng@hellogroup.com>
2025-09-05 09:34:31 +0800

    add

--- src/代码随想录/哈希表/242. 有效的字母异位词.go
+++ src/代码随想录/哈希表/242. 有效的字母异位词.go
@@ -1,6 +1,6 @@
 package main
 
-func isAnagram(s string, t string) bool {
+func isAnagram1(s string, t string) bool {
 
 	m := make(map[byte]int, len(s))
 	for i := 0; i < len(s); i++ {
@@ -9,6 +9,26 @@ func isAnagram(s string, t string) bool {
 
 	for i := 0; i < len(t); i++ {
 		m[t[i]]--
+	}
+
+	for _, v := range m {
+		if v != 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func isAnagram(s string, t string) bool {
+
+	m := [26]int{}
+	for i := 0; i < len(s); i++ {
+		m[s[i]-97]++
+	}
+
+	for i := 0; i < len(t); i++ {
+		m[t[i]-97]--
 	}
 
 	for _, v := range m {
