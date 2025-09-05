1c750554917506617b21a9ee505470e6170738ea
renpeng <renpeng@hellogroup.com>
2025-09-05 14:14:45 +0800

    add

--- src/代码随想录/哈希表/15. 三数之和.go
+++ src/代码随想录/哈希表/15. 三数之和.go
@@ -9,7 +9,6 @@ func threeSum(nums []int) [][]int {
 	for i, v := range nums {
 		m[v] = i
 	}
-	// existed := make(map[[3]int]struct{})
 	res := make([][]int, 0, 8)
 	for i := 0; i < len(nums); i++ {
 		if i > 0 && nums[i] == nums[i-1] {
@@ -17,14 +16,10 @@ func threeSum(nums []int) [][]int {
 		}
 		for j := i + 1; j < len(nums); j++ {
 
-			if i > i+1 && nums[j] == nums[j-1] {
+			if j > i+1 && nums[j] == nums[j-1] {
 				continue
 			}
 			if index, ok := m[0-nums[i]-nums[j]]; ok && index > j {
-				// if _, existedOk := existed[[3]int{nums[i], nums[j], nums[index]}]; !existedOk {
-				// 	res = append(res, []int{nums[i], nums[j], nums[index]})
-				// 	existed[[3]int{nums[i], nums[j], nums[index]}] = struct{}{}
-				// }
 				res = append(res, []int{nums[i], nums[j], nums[index]})
 			}
 		}
