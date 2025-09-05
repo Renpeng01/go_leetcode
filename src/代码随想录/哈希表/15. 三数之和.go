0b5dc9b576e93e7bca45af6113546bef886e5409
renpeng <renpeng@hellogroup.com>
2025-09-05 10:33:56 +0800

    add

--- src/代码随想录/哈希表/15. 三数之和.go
+++ src/代码随想录/哈希表/15. 三数之和.go
@@ -9,17 +9,23 @@ func threeSum(nums []int) [][]int {
 	for i, v := range nums {
 		m[v] = i
 	}
-
-	existed := make(map[[3]int]struct{})
-
+	// existed := make(map[[3]int]struct{})
 	res := make([][]int, 0, 8)
 	for i := 0; i < len(nums); i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
 		for j := i + 1; j < len(nums); j++ {
+
+			if i > i+1 && nums[j] == nums[j-1] {
+				continue
+			}
 			if index, ok := m[0-nums[i]-nums[j]]; ok && index > j {
-				if _, existedOk := existed[[3]int{nums[i], nums[j], nums[index]}]; !existedOk {
-					res = append(res, []int{nums[i], nums[j], nums[index]})
-					existed[[3]int{nums[i], nums[j], nums[index]}] = struct{}{}
-				}
+				// if _, existedOk := existed[[3]int{nums[i], nums[j], nums[index]}]; !existedOk {
+				// 	res = append(res, []int{nums[i], nums[j], nums[index]})
+				// 	existed[[3]int{nums[i], nums[j], nums[index]}] = struct{}{}
+				// }
+				res = append(res, []int{nums[i], nums[j], nums[index]})
 			}
 		}
 	}
