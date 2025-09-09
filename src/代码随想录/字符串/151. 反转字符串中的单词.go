ecf0edd905a9a299f0f0f665912a1d3cb54f1fd8
renpeng <renpeng@hellogroup.com>
2025-09-09 09:57:30 +0800

    add

--- src/代码随想录/字符串/151. 反转字符串中的单词.go
+++ src/代码随想录/字符串/151. 反转字符串中的单词.go
@@ -3,13 +3,32 @@ package main
 import "strings"
 
 func reverseWords(s string) string {
-	s = strings.TrimSpace(s)
-	words := strings.Split(s, " ")
+
+	words := make([]string, 0, 16)
+
+	word := make([]byte, 0, 16)
+	for i := 0; i < len(s); i++ {
+		if s[i] == ' ' {
+			if len(word) > 0 {
+				words = append(words, string(word))
+				word = make([]byte, 0, 16)
+				continue
+			}
+			continue
+		}
+		word = append(word, s[i])
+	}
+
+	if len(word) > 0 {
+		words = append(words, string(word))
+	}
 
 	left := 0
 	right := len(words) - 1
 	for left < right {
 		words[left], words[right] = words[right], words[left]
+		left++
+		right--
 	}
 	return strings.Join(words, " ")
 }
