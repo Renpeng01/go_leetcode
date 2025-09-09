71d2caa19065d07601936616ed9003f4579bfa11
renpeng <renpeng@hellogroup.com>
2025-09-09 17:03:09 +0800

    add

--- src/代码随想录/栈与队列/150. 逆波兰表达式求值.go
+++ src/代码随想录/栈与队列/150. 逆波兰表达式求值.go
@@ -1,11 +1,13 @@
 package main
 
-import "strconv"
+import (
+	"fmt"
+	"strconv"
+)
 
 func evalRPN(tokens []string) int {
 	nums := make([]int, 0, len(tokens))
 	for _, v := range tokens {
-
 		switch v {
 		case "+":
 			v1 := nums[len(nums)-1]
@@ -16,7 +18,7 @@ func evalRPN(tokens []string) int {
 			v1 := nums[len(nums)-1]
 			v2 := nums[len(nums)-2]
 			nums = nums[:len(nums)-2]
-			nums = append(nums, v1-v2)
+			nums = append(nums, v2-v1)
 		case "*":
 			v1 := nums[len(nums)-1]
 			v2 := nums[len(nums)-2]
@@ -26,11 +28,19 @@ func evalRPN(tokens []string) int {
 			v1 := nums[len(nums)-1]
 			v2 := nums[len(nums)-2]
 			nums = nums[:len(nums)-2]
-			nums = append(nums, v1/v2)
+			nums = append(nums, v2/v1)
 		default:
 			n, _ := strconv.Atoi(v)
 			nums = append(nums, n)
 		}
+		// fmt.Println(nums)
 	}
 	return nums[0]
+}
+
+func main() {
+	// tokens := []string{"2", "1", "+", "3", "*"}
+	tokens := []string{"4", "13", "5", "/", "+"}
+	res := evalRPN(tokens)
+	fmt.Println(res)
 }
