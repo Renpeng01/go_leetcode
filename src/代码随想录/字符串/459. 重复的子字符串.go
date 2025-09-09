67888aa9bd08a02d7e4663c41918992d0c0702de
renpeng <renpeng@hellogroup.com>
2025-09-09 15:04:24 +0800

    add

--- src/代码随想录/字符串/459. 重复的子字符串.go
+++ src/代码随想录/字符串/459. 重复的子字符串.go
@@ -1,21 +1,20 @@
 package main
 
 func repeatedSubstringPattern(s string) bool {
-
 	n := len(s)
 	for step := 1; step <= n/2; step++ {
 		if n%step != 0 {
 			continue
 		}
+
 		for j := 0; j < len(s); j = j + step {
 			if s[:step] != s[j:j+step] {
 				break
 			}
-			if j == len(s)-1-step {
+			if j == len(s)-step {
 				return true
 			}
 		}
 	}
 	return false
-
 }
