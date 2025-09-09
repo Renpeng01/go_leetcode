47d55d1842cd3b5cb6b655537e4d0d31bebeb56b
renpeng <renpeng@hellogroup.com>
2025-09-09 17:41:32 +0800

    add

--- src/代码随想录/栈与队列/347. 前 K 个高频元素.go
+++ src/代码随想录/栈与队列/347. 前 K 个高频元素.go
@@ -1,27 +1,5 @@
 package main
 
-import "sort"
-
 func topKFrequent(nums []int, k int) []int {
-	sort.Ints(nums)
-	res := make([]int, 0, 16)
-	cnt := 0
-	for i := 0; i < len(nums); i++ {
-		if len(res) == 0 {
-			res = append(res, nums[i])
-			cnt++
-			continue
-		}
 
-		if cnt == k {
-			break
-		}
-
-		if nums[i] == res[len(res)-1] {
-			continue
-		}
-		res = append(res, nums[i])
-		cnt++
-	}
-	return res
 }
