363f565f85abf45af8ee5fcfdbff00699aba703e
renpeng <renpeng@hellogroup.com>
2025-09-09 09:15:37 +0800

    add

---
+++ src/代码随想录/字符串/541. 反转字符串 II.go
@@ -0,0 +1,18 @@
+package main
+
+func reverseStr(s string, k int) string {
+
+	bs := []byte(s)
+	for i := 0; i < len(s); i = i + k - 1 {
+		if (i+1)%k == 0 && ((i+1)/k)%2 == 1 {
+			left := i - (k - 1)
+			right := i
+			for left < right {
+				bs[left], bs[right] = bs[right], bs[left]
+				left++
+				right--
+			}
+		}
+	}
+	return string(bs)
+}
