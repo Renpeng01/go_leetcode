0ee8f1c3e6ecb9ef21e95d1207b7b7b304a60953
renpeng <renpeng@hellogroup.com>
2025-09-09 09:38:48 +0800

    add

--- src/代码随想录/字符串/541. 反转字符串 II.go
+++ src/代码随想录/字符串/541. 反转字符串 II.go
@@ -7,12 +7,11 @@ func reverseStr(s string, k int) string {
 	}
 
 	bs := []byte(s)
-
-	n := len(s) / (2 * k)
-
+	step := 2 * k
+	n := len(s) / step
 	for i := 0; i < n; i++ {
-		left := i * (2 * k)
-		right := i*(2*k) + k - 1
+		left := i * step
+		right := i*step + k - 1
 		for left < right {
 			bs[left], bs[right] = bs[right], bs[left]
 			left++
@@ -20,13 +19,12 @@ func reverseStr(s string, k int) string {
 		}
 	}
 
-	if len(s)%(2*k) > 0 {
-		left := (n - 1) * (2 * k)
-		right := (n-1)*(2*k) + k - 1
-		if len(s)%(2*k) <= k {
+	if len(s)%step > 0 {
+		left := (n - 1) * step
+		right := (n-1)*step + k - 1
+		if len(s)%step <= k {
 			right = len(s) - 1
 		}
-
 		for left < right {
 			bs[left], bs[right] = bs[right], bs[left]
 			left++
