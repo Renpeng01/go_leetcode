918bf68ccb586721cd03a29a65c085c562dc51ef
renpeng <renpeng@hellogroup.com>
2026-01-19 20:21:21 +0800

    add

--- src/代码随想录/图/797. 所有可能的路径.go
+++ src/代码随想录/图/797. 所有可能的路径.go
@@ -1,50 +1,30 @@
 package main
 
 var res [][]int
-var existed map[int]struct{}
 var path []int
-var N int
 
 func allPathsSourceTarget(graph [][]int) [][]int {
 	res = make([][]int, 0, 16)
-	existed = make(map[int]struct{}, 16)
+
 	path = make([]int, 0, 16)
-
-	// n为目的地
-	N = -1
-	for k, v := range graph {
-		if k > N {
-			N = k
-		}
-		for _, s := range v {
-			if s > N {
-				N = s
-			}
-		}
-	}
-
+	path = append(path, 0)
 	dfs(0, graph)
 	return res
 }
 
 func dfs(cur int, graph [][]int) {
-	if cur == N {
-		path = append(path, cur)
-		res = append(res, path)
-		path = make([]int, 0, 16)
+	if cur == len(graph)-1 {
+		tmp := make([]int, len(path))
+		copy(tmp, path)
+		res = append(res, tmp)
 		return
 
 	}
-	_, ok := existed[cur]
-	if len(graph[cur]) == 0 || ok {
-		return
-	}
 
-	path = append(path, cur)
-	existed[cur] = struct{}{}
 	for _, p := range graph[cur] {
+		path = append(path, p)
 		dfs(p, graph)
+		path = path[:len(path)-1]
 	}
-	path = path[:len(path)-1]
-	delete(existed, cur)
+
 }
